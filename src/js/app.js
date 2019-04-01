245d742f4ef46aff25669146a04f94672827a690
Ashwani Kumar <ashwani.bietjhs@gmail.com>
2019-04-01 17:11:58 +0530

    webpack fixes

--- src/js/app.js
+++ src/js/app.js
@@ -1,55 +1,55 @@
 const header = `<nav>
-<div class="row">
-    <a href="#">
-        <img src="assets/images/logo.png" alt="Sabka Bazaar" srcset="" class="logo">
-    </a>
-    <div>
-        <span class="cart_btn">
-            <a href="#" class="btn_to_cart">
-                <i class="cart_icon">
-                    <svg version="1.1" width="30" id="Layer_1" focusable="false"
-                        xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-                        x="0px" y="0px" viewBox="0 0 24 24" style="enable-background:new 0 0 24 24;"
-                        xml:space="preserve">
-                        <path
-                            d="M7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2zM1 2v2h2l3.6 7.59-1.35 2.45c-.16.28-.25.61-.25.96 0 1.1.9 2 2 2h12v-2H7.42c-.14 0-.25-.11-.25-.25l.03-.12.9-1.63h7.45c.75 0 1.41-.41 1.75-1.03l3.58-6.49A1.003 1.003 0 0 0 20 4H5.21l-.94-2H1zm16 16c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2 2-.9 2-2-.9-2-2-2z" />
-                    </svg>
-                    <span>0</span>
-                </i>
-                <span>0 items</span>
-            </a>
-        </span>
-        <div class="nav_btn">
-            <label for="hamberger">
-                <span></span>
-                <span></span>
-                <span></span>
-            </label>
-        </div>
-        <input type="checkbox" id="hamberger" />
-        <div class="nav_link">
-            <ul>
-                <li><a href="index.html">Home</a></li>
-                <li><a href="#">Products</a></li>
-            </ul>
-            <ul>
-                <li><a href="#">SignIn</a></li>
-                <li><a href="#">Register</a></li>
-            </ul>
-        </div>
-    </div>
-</div>
-</nav>`;
+                <div class="row">
+                    <a href="#">
+                        <img src="/dist/images/logo.png" alt="Sabka Bazaar" srcset="" class="logo">
+                    </a>
+                    <div>
+                        <span class="cart_btn">
+                            <a href="#" class="btn_to_cart">
+                                <i class="cart_icon">
+                                    <svg version="1.1" width="30" id="Layer_1" focusable="false"
+                                        xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                                        x="0px" y="0px" viewBox="0 0 24 24" style="enable-background:new 0 0 24 24;"
+                                        xml:space="preserve">
+                                        <path
+                                            d="M7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2zM1 2v2h3l3.6 7.59-1.35 2.45c-.16.28-.25.61-.25.96 0 1.1.9 2 2 2h12v-2H7.42c-.14 0-.25-.11-.25-.25l.03-.12.9-1.63h7.45c.75 0 1.41-.41 1.75-1.03l3.58-6.49A1.003 1.003 0 0 0 20 4H5.21l-.94-2H1zm16 16c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2 2-.9 2-2-.9-2-2-2z" />
+                                    </svg>
+                                    <span>0</span>
+                                </i>
+                                <span>0 items</span>
+                            </a>
+                        </span>
+                        <div class="nav_btn">
+                            <label for="hamberger">
+                                <div class="hamberger"></div>
+                                <div class="hamberger"></div>
+                                <div class="hamberger"></div>
+                            </label>
+                        </div>
+                        <input type="checkbox" id="hamberger" />
+                        <div class="nav_link">
+                            <ul>
+                                <li><a href="index.html">Home</a></li>
+                                <li><a href="products.html">Products</a></li>
+                            </ul>
+                            <ul>
+                                <li><a href="login.html">SignIn</a></li>
+                                <li><a href="signup.html">Register</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                </nav>`;
 
 
-const footer = ``;
+const footer = `<section class="row">
+                <p>
+                    Copyright &copy; 2011 &mdash; 2018 Sabka Bazaar Grocery Supplies Pvt Ltd
+                </p>
+                </section>`;
 
 (function () {
-    console.log("hello");
-
-    //document.getElementById("header").innerHTML = header;
-
-
-
+    document.getElementById("header").innerHTML = header;
+    document.getElementById("footer").innerHTML = footer;
 })();
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,20 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const cssOutput = 'css/style.css';
 
 module.exports = {
     entry: [
-        "./src/js/app.js", 
+        "./src/js/app.js",
+        "./src/scss/products.scss",
+        "./src/scss/login.scss",
         "./src/scss/carousel.scss",
         "./src/scss/query.scss",
-        "./src/scss/home.scss"        
+        "./src/scss/home.scss"
     ],
     output: {
-        path: path.resolve(__dirname, "dist/assets"),
+        path: path.resolve(__dirname, "dist"),
         filename: "bundle.js"
     },
     module: {
@@ -31,6 +34,26 @@ module.exports = {
         ]
     },
     plugins: [
-        new ExtractTextPlugin(cssOutput)
+        new ExtractTextPlugin(cssOutput),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            inject: true,
+            template: path.resolve(__dirname, "src/index.html")
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'login.html',
+            inject: true,
+            template: path.resolve(__dirname, "src/login.html")
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'products.html',
+            inject: true,
+            template: path.resolve(__dirname, "src/products.html")
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'signup.html',
+            inject: true,
+            template: path.resolve(__dirname, "src/signup.html")
+        })
     ]
 };
