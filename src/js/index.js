fc4ceb055fd26cc98701d173aa2546d588e4e442
Subhita Choudhary <subchoud@publicisgroupe.net>
2019-04-03 13:22:58 +0530

    Added html and sass

---
+++ src/js/index.js
@@ -0,0 +1,1 @@
+// import './../css/sass/main.scss'
---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+
+
+
+module.exports = {
+
+  entry: [
+    './src/css/sass/main.scss',
+    './src/js/index.js',
+  ],
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+
+  plugins: [
+
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    }),
+
+    new CopyWebpackPlugin([
+      { from: 'src/images', to: 'img' }
+    ]),
+
+    new HtmlWebpackPlugin(
+
+      {  
+        template: path.resolve(__dirname, './src/views/index.html'),
+        filename: 'index.html',
+        inject: true
+      }
+    ),
+    new HtmlWebpackPlugin(
+
+      {  
+        template: path.resolve(__dirname, './src/views/product-list.html'),
+        filename: 'product-list.html',
+        inject: true
+      }
+    ),
+    new HtmlWebpackPlugin(
+
+      {  
+        template: path.resolve(__dirname, './src/views/login.html'),
+        filename: 'login.html',
+        inject: true
+      }
+
+    ),
+    new HtmlWebpackPlugin(
+
+      {  
+        template: path.resolve(__dirname, './src/views/register.html'),
+        filename: 'register.html',
+        inject: true
+      }
+    )
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  }
+}
