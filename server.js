827cfe98756e8a019ddcbedd467da586ef3b4723
Mohit Jain <mohjain6@publicisgroupe.net>
2019-04-03 12:32:22 +0530

    Css refactor

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+// data for the views
+const categories = require('./data/categories');
+const products = require('./data/products');
+
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/resources/views'));
@@ -11,7 +15,7 @@ app.set('views', path.join(__dirname, '/resources/views'));
 app.use('/resources', express.static('resources'));
 
 // use res.render to load up an ejs view file
-// index page
+
 app.get('/', function(_, res) {
   res.render('pages/index', require('./data/signUp'));
 });
@@ -22,23 +26,21 @@ app.get('/signin', function(_, res) {
 
 app.get('/home', function(_, res) {
   res.render('pages/home', {
-    categories: require('./data/categories').filter(val => val.imageUrl)
+    categories: categories.filter(val => val.imageUrl)
   });
 });
 
-// about page
 app.get('/products/:key?', function(req, res) {
-  let key = req.params.key;
-  let categories = require('./data/categories');
-  let products = require('./data/products');
-  let category = categories.find(val => val.key === key);
-  let categoryId = category && category.id;
+  const key = req.params.key;
+  const category = categories.find(val => val.key === key);
+  const categoryId = category && category.id;
+  let productsData;
   if (categoryId) {
-    products = products.filter(val => val.category === categoryId);
+    productsData = products.filter(val => val.category === categoryId);
   }
   res.render('pages/products', {
-    categories: require('./data/categories'),
-    products,
+    categories,
+    products: productsData,
     categoryId
   });
 });
