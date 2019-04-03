5bd9a09c27e976f707f63a064054ac0ac28aebf7
Shubhankar Mayank <shubhankarmayank@gmail.com>
2019-04-03 19:48:16 +0530

    UI - ALL- pages
    All pages complete and responsive
    ------Known Defects-------
    1) No Hamburger button for mobile view
    2)Proceed to checkout button in cart not in bottom
    3)PLP page navigation not expanding to full height

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,31 @@
 import styles from '../styles/app.module'
 import '../styles/global'
 
-$(".shopping-cart").fadeToggle( "fast");
-(function(){
- 
-    $("#cart").on("click", function() {
-      $(".shopping-cart").fadeToggle( "fast");
-      
-    });
-    
-  })();
+
+
+
+  // Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("cart");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
