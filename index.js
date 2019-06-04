1b44e0e52f03af3977a3d132312a7ec4ee80823d
samreenzaidi <36235936+samreenzaidi@users.noreply.github.com>
2019-06-04 14:35:13 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var path = require('path');
-var hbs = require('express-handlebars');
-var app = express();
-
-app.engine('hbs',hbs({extname: 'hbs', defaultLayout:'layout', layoutDir: __dirname+'/views/pages/'}));
-app.set('views',path.join(__dirname,'views/pages'));
-app.set('view engine','hbs');
