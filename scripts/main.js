a62fc2b3d3d8139db4750f792fd96f3cb454e89c
Shipra94 <51152581+Shipra94@users.noreply.github.com>
2019-06-14 15:46:15 +0530

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -6,10 +6,18 @@ let obj = fs.readFileSync('./server/banners/index.get.json');
 let banners = JSON.parse(obj);
 let obj1 = fs.readFileSync('./server/categories/index.get.json');  
 let categories = JSON.parse(obj1);
+let productObj = fs.readFileSync('./server/products/index.get.json'); 
+let products = JSON.parse(productObj);
 routing.get('/', function (req, res, next) {
     res.render('home', {
         banners: banners.banners,
         categories: categories.categories
     });  
 }); 
+routing.get('/products', function(req, res,next){
+    res.render('products',{
+        categories: categories.categories,
+        products: products.products
+    });
+});
 module.exports = routing;
