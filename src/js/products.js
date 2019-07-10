cf3ecc17d8df5b42bfbf67682e8d0cfe9b3333fe
Vinay Garg <vinaygarg502@gmail.com>
2019-07-10 09:56:53 +0530

    added responsiveness for category block in plp page

--- src/js/products.js
+++ src/js/products.js
@@ -1,4 +1,41 @@
 import './../utils/styles/global.scss';
 import './../styles/products.scss';
 
-console.log('hi');
+function NavToggle(){
+	var toggleBtn =  document.querySelector('.nav-icon--mobile');
+	toggleBtn.addEventListener('click',toggleNav);
+	function toggleNav(){
+		var i = document.querySelector('.nav-icon--mobile em');
+		if(i.classList.contains('icofont-navigation-menu')){
+			i.classList.remove('icofont-navigation-menu');
+			this.parentNode.classList.add('active');
+			i.classList.add('icofont-close');
+		}
+		else{
+			i.classList.remove('icofont-close');
+			this.parentNode.classList.remove('active');
+			i.classList.add('icofont-navigation-menu');	
+		}
+	}
+}
+NavToggle();
+
+function toggleCat(){
+	var toggleBtn =  document.querySelector('.category-mobile');
+	var ul = document.querySelector('.category-block');
+	toggleBtn.addEventListener('click',toggleNav);
+	ul.addEventListener('click', selectCategory);
+	function toggleNav(){
+		if(ul.classList.contains('active')){
+			ul.classList.remove('active');
+		}
+		else{
+			ul.classList.add('active');
+		}
+	}
+	function selectCategory(e){
+		toggleBtn.innerText= e.target.innerText;
+		ul.classList.remove('active');
+	}
+}
+toggleCat();
