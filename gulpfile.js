0814b6f4dd24f1e53010c839d0f798181d32803d
Rustam Ali <rusali@publicisgroupe.net>
2019-08-30 12:40:48 +0530

    some css convert in scss

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,7 @@ gulp.task('default', function(){
     console.log("default task");
 });
 
+// Add task to compile scss code inot css code
 gulp.task('scss-css', function(){
     gulp.src(settings.sources.scss)
         .pipe(sass().on('error', sass.logError))
