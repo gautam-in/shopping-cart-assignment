28652e0fa2225a73176fcad3aaaec943924406fe
Rohit Sharma <rohitgargi0293@gmail.com>
2019-12-16 23:36:43 +0530

    Added interval for image slider and outline style removed for common button and cart button

--- src/app/home/components/carousel.component.ts
+++ src/app/home/components/carousel.component.ts
@@ -1,25 +1,30 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'shopping-cart-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.scss']
 })
-export class CarouselComponent implements OnInit {
+export class CarouselComponent implements OnInit, OnDestroy{
   @Input('bannerList') bannerList:any;
+  interval : any;
   slideIndex:number = 1;
-  constructor() { }
+  constructor(private el : ElementRef) { }
 
   ngOnInit() {
+    this.plusSlides(1);
+    this.interval = setInterval(()=>{
+      this.plusSlides(1);
+    },2000)
   }
 
-  showSlides(n) {
+  showSlides() {
     let i:number;
-    let slides:any = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
+    let slides:any = this.el.nativeElement.querySelectorAll('.mySlides');
+    let dots = this.el.nativeElement.querySelectorAll(".dot");
 
-    if (n > slides.length) {this.slideIndex = 1}    
-    if (n < 1) {this.slideIndex = slides.length}
+    if (this.slideIndex > slides.length) {this.slideIndex = 1}    
+    if (this.slideIndex < 1) {this.slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
@@ -34,10 +39,14 @@ export class CarouselComponent implements OnInit {
     
   }
   currentSlide(n) {
-    this.showSlides(this.slideIndex = n);
+    this.slideIndex = n;
+    this.showSlides();
   }
   plusSlides(n) {
-    this.showSlides(this.slideIndex += n);
+    this.slideIndex += n
+    this.showSlides();
   }
-
+  ngOnDestroy(){
+    clearInterval(this.interval);
+  }
 }
