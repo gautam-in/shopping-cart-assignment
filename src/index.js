128e54b3b3c236f059dc3d3714498accb135e02c
Himanshu Sharma <himsharm9@publicisgroupe.net>
2020-01-31 18:23:30 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,2 @@
-import _ from 'lodash'
-
-const component = () => {
-  const element = document.createElement('div')
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ')
-  return element
-}
-
-document.body.appendChild(component())
+console.log('hello')
+console.error('');
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,42 +1,43 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const path = require('path')
 const rootDir = path.resolve(__dirname, '.')
 const srcDir = path.resolve(__dirname, '.', 'src')
 const distDir = path.resolve(__dirname, '.', 'dist')
-module.exports = {
-  entry: {
-    index:srcDir,
-  },
-  devServer: {
-    contentBase: distDir,
-    compress: true,
-    port: 9000
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(sass|scss)$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
-      },
-    ],
-  },
-  pulugins:[
-    new HtmlWebPackPlugin({
-      // where to find the html template
-      template: path.resolve(__dirname, 'index.html'),
-      // where to put the generated file
-      path: distDir,
-      // the output file name
-      filename: 'index.html',
-      files: {
-        css: ['styles.css'],
-        js: ['bundle.js']
-      }
-    }),],
+module.exports = () => {
+  return {
+    context: rootDir,
+    entry: {
+      index: srcDir
+    },
     output: {
       path: distDir,
       publicPath: '/',
       filename: '[hash].js'
-    }
-  };
-  
+    },
+    devServer: {
+      contentBase: [path.join(__dirname, 'server'), path.join(__dirname, 'static')],
+      publicPath: '/',
+      historyApiFallback: true,
+      hot: true,
+      open: true,
+      // needed for phone testing
+    },
+    devServer.port:'4000',
+    devtool: 'cheap-module-source-map',
+    plugins: [
+      new HtmlWebPackPlugin({
+        // where to find the html template
+        template: path.resolve(__dirname, 'index.html'),
+        // where to put the generated file
+        path: distDir,
+        // the output file name
+        filename: 'index.html',
+        files: {
+          css: ['styles.css'],
+          js: ['bundle.js']
+        }
+      })
+    ],
+    mode: devMode ? 'development' : 'production'
+  }
+}
