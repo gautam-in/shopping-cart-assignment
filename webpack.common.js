de15a847f6a6eb6a86729fc5c80ee7254d20c5d7
Himanshu Sharma <himsharm9@publicisgroupe.net>
2020-02-05 16:21:09 +0530

    webpack change

--- webpack.common.js
+++ webpack.common.js
@@ -1,102 +1,91 @@
-const glob = require('glob');
-const path = require('path');
-const webpack = require('webpack')
-
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const dotenv = require('dotenv-webpack');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const ExtractCssChunks = require('extract-css-chunks-webpack-plugin');
-
+const glob = require('glob')
+const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const ExtractCssChunks = require('extract-css-chunks-webpack-plugin')
+// const dotenv = require('dotenv-webpack')
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const generateHTMLPlugins = () => glob.sync('./src/**/*.html').map(
   dir => new HTMLWebpackPlugin({
     filename: path.basename(dir),
     template: dir,
   }),
-);
+)
 
-module.exports = () => {
-  const env = dotenv.config().parsed
-  const envKeys = Object.keys(env).reduce((prev, next) => {
-    prev[`process.env.${next}`] = JSON.stringify(env[next])
-    return prev
-  }, {})
-  return {
-    node: {
-      fs: 'empty',
-    },
-    entry: ['./src/js/app.js', './src/style/main.scss'],
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'app.bundle.js',
-    },
-    performance: { hints: false },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          loader: 'babel-loader',
-        },
-        {
-          test: /\.html$/,
-          loader: 'raw-loader',
-        },
-        {
-          test: /\.(pdf|gif|png|jpe?g|svg)$/,
-          use: [
-            {
-              loader: 'file-loader',
-              options: {
-                outputPath: 'static/',
-              },
-            },
-          ],
-        },
-        {
-          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-          use: [{
+module.exports = {
+  node: {
+    fs: 'empty',
+  },
+  entry: ['./src/js/app.js', './src/style/main.scss'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js',
+  },
+  performance: { hints: false },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.html$/,
+        loader: 'raw-loader',
+      },
+      {
+        test: /\.(pdf|gif|png|jpe?g|svg)$/,
+        use: [
+          {
             loader: 'file-loader',
             options: {
-              name: '[name].[ext]',
-              outputPath: 'fonts/',
+              outputPath: 'static/',
             },
-          }],
-        },
-        {
-          test: /\.s[ac]ss$/i,
-          use: [
-            'style-loader',
-            'css-loader',
-            'sass-loader',
-          ],
-        },
-      ],
-    },
-    devServer: {
-      contentBase: path.join(__dirname, 'src'),
-      hot:true,
-      port:3000
-    },
-    plugins: [
-      new CopyWebpackPlugin([
-        {
-          from: './src/static/',
-          to: './static/',
-        },
-      ]),
-      new ExtractCssChunks(
-        {
-          filename: '[name].css',
-          chunkFilename: '[id].css'
-        }
-      ),
-      new webpack.DefinePlugin(envKeys),
-      ...generateHTMLPlugins(),
+          },
+        ],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/',
+          },
+        }],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
     ],
-    
-    stats: {
-      colors: true,
-    },
-    devtool: 'source-map',
-  }
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'src'),
+    hot:true,
+    port:3000
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: './src/static/',
+        to: './static/',
+      },
+    ]),
+    new ExtractCssChunks(
+      {
+        filename: '[name].css',
+        chunkFilename: '[id].css'
+      }
+    ),
+    ...generateHTMLPlugins(),
+  ],
+  
+  stats: {
+    colors: true,
+  },
+  devtool: 'source-map',
 }
