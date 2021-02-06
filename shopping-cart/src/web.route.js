3ababb3c184faea0a4dd4b55f8137a89442077eb
Alpana Majhi <alpmajhi@publicisgroupe.net>
2021-02-06 16:48:54 +0530

    Changed to hash router

--- shopping-cart/src/web.route.js
+++ shopping-cart/src/web.route.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './container/Home/Home';
 import Signin from './components/SignIn/Signin';
 import Products from './components/Products/Products.jsx';
