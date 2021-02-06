65afc9e9ab0f21ba4de0e8800223497e4b3be79a
Neetu Chauhan <neechauh1@publicisgroupe.net>
2021-02-07 00:47:41 +0530

    Login and SignUp added

--- client/src/components/login/index.js
+++ client/src/components/login/index.js
@@ -1,9 +1,56 @@
 import React from 'react';
+import './index.scss';
+import { Link } from 'react-router-dom';
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            
+                <div class='card'>
+                    <div className='container'>
+                        <div className="row">
+                            <div className='returnHome'>
+                                <Link to='/' className='link'>Return Home</Link>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="text-center">
+                                <h3>
+                                    Login Below
+                              </h3>
+                                <p>Don't have an account? <Link to="/register" className="link">Register</Link></p>
+                            </div>
 
-const index = () => {
-    return <div>
-vhgggf
-    </div>
+                        </div>
+                        <form>
+                            <div className='row'>
+                                <div className="col-8 offset-2">
+                                    <form class="login-form">
+                                        <input type="text" placeholder="Enter your Email" />
+                                    </form>
 
+                                </div>
+
+                                <div className="col-8 offset-2">
+                                    <form class="login-form">
+                                        <input type="text" placeholder="Enter your passowrd" />
+                                    </form>
+
+                                </div>
+
+                            </div>
+                            <div className="col-8 offset-2">
+                                <button className="btn loginbtn" type="submit">
+                                    Log In
+			 	  				</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+           
+        )
+    }
 }
-export default index;
+export default Login;
--- client/src/components/signUp/index.js
+++ client/src/components/signUp/index.js
@@ -1,9 +1,70 @@
 import React from 'react';
+import './index.scss';
 
-const index = () => {
-    return <div>
-nndjnmd
-    </div>
+import { Link } from 'react-router-dom';
+class Register extends React.Component {
+    constructor(props) {
+        super(props);
 
+    }
+    render() {
+        return (
+            <div className='card1'>
+                <div class='card'>
+                    <div className='container'>
+                        <div className="row">
+                            <div className='returnHome'>
+                                <Link to='/' className='link'>Return Home</Link>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="text-center">
+                                <h3>
+                                    Login In
+              </h3>
+                                <p>Already have an account? <Link to="/login" className="link">Log In</Link></p>
+                            </div>
+
+                        </div>
+                        <form>
+                            <div className='row'>
+                                <div className="col-8 offset-2">
+                                    <form class="login-form">
+                                        <input type="text" placeholder="Enter your name" />
+                                    </form>
+
+                                </div>
+
+                                <div className="col-8 offset-2">
+                                    <form class="login-form">
+                                        <input type="text" placeholder="Enter your Email" />
+                                    </form>
+
+                                </div>
+                                <div className="col-8 offset-2">
+                                    <form class="login-form">
+                                        <input type="text" placeholder="Enter your passowrd" />
+                                    </form>
+
+                                </div>
+                                <div className="col-8 offset-2">
+                                    <form class="login-form">
+                                        <input type="text" placeholder="Re-Type your passowrd" />
+                                    </form>
+
+                                </div>
+
+                            </div>
+                            <div className="col-8 offset-2">
+                                <button className="btn signupbtn" type="submit">
+                                    Sign Up
+			 	  				</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
-export default index;
+export default Register;
