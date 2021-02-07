cb2d9bf57210fa382fa4e7edd4d108b3c7a52c2d
Alpana Majhi <alpmajhi@publicisgroupe.net>
2021-02-07 11:32:34 +0530

    Carousel in progress

--- src/container/Home/Carousel.jsx
+++ src/container/Home/Carousel.jsx
@@ -6,72 +6,85 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 
-const processLeftMedia = (cardContentH5, cardContentSubtitle, image, buttonText) => {
-    return (
-        <>
-            <CardMedia style={{ marginRight: '5%' }}>
-                <img style={{ width: '60%',backgroundSize:'contain' }} src={image} alt={buttonText} />
-            </CardMedia>
-            <CardContent style={{ marginTop: '5%', paddingLeft: '2%' }}>
-                <Typography component="h5" variant="h5">
-                    {cardContentH5}
-                </Typography>
-                <Typography variant="subtitle1" color="textSecondary">
-                    {cardContentSubtitle}
-                </Typography>
-                <Button variant="contained" color="secondary" style={{ textTransform: 'none', marginTop: '5%' }}>
-                    {buttonText}
-                </Button>
-            </CardContent>
-        </>
-    );
-}
 
-const processRightMedia = (cardContentH5, cardContentSubtitle, image, buttonText) => {
-    return (
-        <>
-            <CardContent style={{ marginTop: '5%', paddingLeft: '2%' }}>
-                <Typography component="h5" variant="h5">
-                    {cardContentH5}
-                </Typography>
-                <Typography variant="subtitle1" color="textSecondary">
-                    {cardContentSubtitle}
-                </Typography>
-                <Button variant="contained" color="secondary" style={{ textTransform: 'none', marginTop: '5%' }}>
-                    {buttonText}
-                </Button>
-            </CardContent>
-            <CardMedia>
-                <img style={{ width: '70%' }} src={image} alt={buttonText} />
-            </CardMedia>
-        </>
-    );
-}
+class Cards extends React.Component {
+    constructor({ props }) {
+        super({ props });
+    }
+    processLeftMedia = (cardContentH5, cardContentSubtitle, image, buttonText) => {
+        return (
+            <>
+                <CardMedia style={{ marginRight: '5%' }}>
+                    <img style={{ width: '60%', backgroundSize: 'contain' }} src={image} alt={buttonText} />
+                </CardMedia>
+                <CardContent style={{ marginTop: '5%', paddingLeft: '2%' }}>
+                    <Typography component="h5" variant="h5">
+                        {cardContentH5}
+                    </Typography>
+                    <Typography variant="subtitle1" color="textSecondary">
+                        {cardContentSubtitle}
+                    </Typography>
+                    <Button variant="contained" color="secondary" style={{ textTransform: 'none', marginTop: '5%' }}>
+                        {buttonText}
+                    </Button>
+                </CardContent>
+            </>
+        );
+    }
 
+    processRightMedia = (cardContentH5, cardContentSubtitle, image, buttonText) => {
+        return (
+            <>
+                <CardContent style={{ marginTop: '5%', paddingLeft: '2%' }}>
+                    <Typography component="h5" variant="h5">
+                        {cardContentH5}
+                    </Typography>
+                    <Typography variant="subtitle1" color="textSecondary">
+                        {cardContentSubtitle}
+                    </Typography>
+                    <Button variant="contained" color="secondary" style={{ textTransform: 'none', marginTop: '5%' }}>
+                        {buttonText}
+                    </Button>
+                </CardContent>
+                <CardMedia>
+                    <img style={{ width: '70%' }} src={image} alt={buttonText} />
+                </CardMedia>
+            </>
+        );
+    }
 
-const Carousel = ({ cardContentH5, cardContentSubtitle, image, mediaPosition, buttonText, lastCard }) => {
-    return (
-        <Container style={{ marginTop: '1%' }}>
-            <Card style={{
-                display: 'flex', marginBottom: '1%',
-                boxShadow: 'none'
-            }}>
-                {mediaPosition % 2 !== 0
-                    ? processLeftMedia(cardContentH5, cardContentSubtitle, image, buttonText)
-                    : processRightMedia(cardContentH5, cardContentSubtitle, image, buttonText)
+    categoryCard = (cardContentH5, cardContentSubtitle, image, mediaPosition, buttonText, lastCard) => {
+        return (
+            <Container style={{ marginTop: '1%' }}>
+                <Card style={{
+                    display: 'flex', marginBottom: '1%',
+                    boxShadow: 'none'
+                }}>
+                    {mediaPosition % 2 !== 0
+                        ? this.processLeftMedia(cardContentH5, cardContentSubtitle, image, buttonText)
+                        : this.processRightMedia(cardContentH5, cardContentSubtitle, image, buttonText)
+                    }
+                </Card >
+                {!lastCard &&
+                    <div style={{
+                        backgroundImage: "radial-gradient(at 48% 0, #9e9898 0%, #9e9898 0%, transparent 70%)",
+                        margin: 'auto',
+                        textAlign: 'center',
+                        height: '8px',
+                        width: '100%',
+                    }}></div>
                 }
-            </Card >
-            {!lastCard &&
-                <div style={{
-                    backgroundImage: "radial-gradient(at 48% 0, #9e9898 0%, #9e9898 0%, transparent 70%)",
-                    margin: 'auto',
-                    textAlign: 'center',
-                    height: '8px',
-                    width: '100%',
-                }}></div>
-            }
-        </Container >
-    );
+            </Container >
+        );
+    }
+    render() {
+        const { cardContentH5, cardContentSubtitle, image, mediaPosition, buttonText, lastCard } = this.props
+        return (
+            <div>
+                {this.categoryCard(cardContentH5, cardContentSubtitle, image, mediaPosition, buttonText, lastCard)}
+            </div>
+        )
+    }
 }
 
-export default Carousel;
+export default Cards;
--- src/container/Home/Home.jsx
+++ src/container/Home/Home.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import Header from '../../components/common/Header'
 import Carousel from './Carousel'
-import Footer from '../../components/common/Footer'
+import Footer from '../../components/common/Footer';
+import './Home.css';
 
+const banners = require('../../server/banners/index.get.json')
 const data = require('../../server/categories/index.get.json')
 
 class Home extends React.Component {
+
+    state = {
+        currentImageIndex: 0
+    };
+
     processCards = () => {
         const length = data.length - 1
         return data.map((val, index) => {
@@ -19,15 +26,70 @@ class Home extends React.Component {
                 lastCard={length === index ? true : false} />
         })
     }
+    processCarousel = () => {
+        return (
+            <div className="container">
+                <div className="carousel">
+                    {this.arrow("left", this.previousSlide, "PREV")}
+                    {banners.map((banner) => {
+                        return this.imageSlide(banner.bannerImageUrl)
+                    })}
+                    {this.arrow("right", this.nextSlide, "NEXT")}
+                </div>
+            </div>);
+    }
 
+    imageSlide = (url) => {
+        console.log(url)
+        const styles = {
+            backgroundImage: `url(${url})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center'
+        };
+
+        return (
+            <div className="image-slide" style={styles}><img src={url} /></div>
+        );
+    }
+
+    arrow = (direction, clickFunction, glyph) => {
+        return (<div
+            className={`slide-arrow ${direction}`}
+            onClick={clickFunction}>
+            { glyph}
+        </div>)
+    };
+
+    previousSlide = () => {
+        const lastIndex = banners.length - 1;
+        const { currentImageIndex } = this.state;
+        const shouldResetIndex = currentImageIndex === 0;
+        const index = shouldResetIndex ? lastIndex : currentImageIndex - 1;
+
+        this.setState({
+            currentImageIndex: index
+        });
+    }
+
+    nextSlide = () => {
+        const lastIndex = banners.length - 1;
+        const { currentImageIndex } = this.state;
+        const shouldResetIndex = currentImageIndex === lastIndex;
+        const index = shouldResetIndex ? 0 : currentImageIndex + 1;
+
+        this.setState({
+            currentImageIndex: index
+        });
+    }
 
     render() {
         return (
             <div>
                 <Header />
+                {/* {this.processCarousel()} */}
                 {this.processCards()}
                 <Footer />
-            </div >
+            </div>
 
         );
     }
