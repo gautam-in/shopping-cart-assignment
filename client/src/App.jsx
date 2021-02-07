aee1737c0fd2398b5d7ce8e22862a5c6bec02746
Neetu Chauhan <neechauh1@publicisgroupe.net>
2021-02-07 11:10:00 +0530

    Icons added and Login changes

---
+++ client/src/App.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route,Switch  } from "react-router-dom"
+import Login from './components/login/index';
+import Register from './components/signUp/index'
+import {IoMdCart} from "react-icons/io"
+import "./App.scss";
+
+class App extends Component { 
+  render(){
+    return (
+      <>
+     <div className="header">
+          <div>
+                 <div className="logo">
+                   <img src="./logo_2x.png"/>
+                   </div>
+                   <div className="navOptions">
+                     <span>
+                       
+                       Home
+                       </span>
+                       <span>
+                         Products
+                       </span>
+                     </div>
+          </div>
+          <div>
+
+      <div className={"cartLogoContainer"}>
+        <div className={"text"}> 
+         <span>
+         SignIn
+           </span>
+           
+           <span>
+           Register
+             </span>  
+          </div>
+          <div className={"cartIcon"}>
+           <div>
+           <IoMdCart style={{color: "#fd032f",fontSize:"2.5rem"}}/>
+             </div> 
+              <div>
+               0 items
+              </div>
+          </div>
+        </div>
+      
+</div>
+       </div>
+      <Router>
+        <Switch>
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+        </Switch>
+    </Router>
+    </>
+    )
+   }
+  }
+
+export default App;
