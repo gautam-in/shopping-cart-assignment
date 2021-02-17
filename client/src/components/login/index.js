586b9249fc0fab6c6689cd7673416ee34fe4ed75
neetu <neetuchauhan0421@gmail.com>
2021-02-18 04:34:33 +0530

    form validation done

--- client/src/components/login/index.js
+++ client/src/components/login/index.js
@@ -1,38 +1,44 @@
 import React from 'react';
 
-import { Link } from 'react-router-dom'; 
+import { Link ,useHistory } from 'react-router-dom';
 import TextField from '@material-ui/core/TextField';
 import './index.scss';
-function Login() { 
- 
-    
+function Login() {
+    const history=useHistory()
    
-        return (
-            <div className={"loginContainer"}>
-                <div>
-                    <span className="login">
-                        Login
-                   </span>
-                    <span className="login2">
-                        Get access to  your Orders,Wishlist and Recommendations
-                   </span>
-                </div>
-                <div>
-                   <form className={"form"}>
-                        <TextField required id="standard-basic" label="Email" />
-                        <TextField required id="standard-basic" label="Password" />
-                        <button class="btn" type="submit">Login</button>
-                   </form>
-
-
-
-                   
-
-
-                </div>
-            </div>
-
-        )
+    function submit(e){
     
+         e.preventDefault();
+
+         history.push("home");
+
+
+    }
+
+    return (
+        <div className={"loginContainer"}>
+            <div>
+                <span className="login">
+                    Login
+                   </span>
+                <span className="login2">
+                    Get access to  your Orders,Wishlist and Recommendations
+                   </span>
+            </div>
+            <div>
+                <form onSubmit={submit} className={"form"}>
+                    <TextField
+                        required id="standard-basic"
+                        label="Email" />
+                    <TextField
+                    type="password"
+                    required id="standard-basic" label="Password" />
+                    <button class="btn" type="submit">Login</button>
+                </form>
+            </div>
+        </div>
+
+    )
+
 }
 export default Login;
