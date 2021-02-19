d5bf364d3048bebc8f4679cb5c63aa069aaf9853
Alpana Majhi <alpmajhi@publicisgroupe.net>
2021-02-20 02:50:38 +0530

    Resolution fixed

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -7,49 +7,76 @@ import buynow from '../../redux/actions/buynow'
 import Snackbar from '@material-ui/core/Snackbar';
 import MuiAlert from '@material-ui/lab/Alert';
 import { withRouter } from 'react-router-dom'
-import './Products.css'
+import './Products.css';
+import Select from '@material-ui/core/Select';
 
 function Alert(props) {
     return <MuiAlert elevation={6} variant="filled" {...props} />;
 }
 
-const data = require('../../server/products/index.get.json');
+// const data = require('../../server/products/index.get.json');
 const resp = require('../../server/addToCart/index.post.json');
-const categories = require('../../server/categories/index.get.json')
+// const categories = require('../../server/categories/index.get.json')
 
 class Products extends React.Component {
 
     state = {
         open: false,
-        categoryData: data,
+        categoryData: "",
         status: false,
-        id: this.props.match.params.id ? this.props.match.params.id : ''
+        id: this.props.match.params.id ? this.props.match.params.id : '',
+        products: [],
+        categories: [],
+        message: ''
+    }
+
+    fetchAPI = (value) => {
+        fetch(`http://localhost:5000/${value}`, {
+            method: 'get'
+        })
+            .then(res => {
+                res.json().then(data => {
+                    if (value === 'products') {
+                        this.setState({ [value]: data, categoryData: data })
+                    } else {
+                        this.setState({ [value]: data })
+                    }
+                })
+            })
+            .catch(err => {
+                console.log(`Cannot find ${value}, Error is: ${err}`)
+            })
     }
     componentDidMount() {
+        this.fetchAPI('products');
+        this.fetchAPI('categories');
         if (this.props.match.params.id) {
-            let categoryData = data.filter(value => {
+            let categoryData = this.state.products.filter(value => {
                 if (this.props.match.params.id && value.category === this.props.match.params.id) {
                     return true
                 }
                 return false
             })
-            this.setState({ categoryData })
+            this.setState({ categoryData, id: this.props.match.params.id })
         } else {
-            this.setState({ categoryData: data })
+            this.setState({ categoryData: this.state.products })
         }
     }
     componentDidUpdate(prevProps) {
-        if (prevProps.count !== undefined && prevProps.count < this.props.count) {
-            this.setState({ open: true }, () => {
-                setInterval(() => {
-                    this.setState({ open: false })
-                }, 3000)
-            })
-        }
+        // if (prevProps.count !== undefined && prevProps.count < this.props.count) {
+        //     this.setState({ open: true }, () => {
+        //         setInterval(() => {
+        //             this.setState({ open: false })
+        //         }, 3000)
+        //     })
+        // }
         if (prevProps.match.path !== this.props.match.path) {
             if (!this.props.match.params.id) {
-                this.setState({ categoryData: data, id: "" })
+                this.setState({ categoryData: this.state.products, id: "" })
             }
+        }
+        if (!this.props.match.params.id && this.state.id) {
+            this.fetchAPI('products')
         }
     }
 
@@ -58,17 +85,15 @@ class Products extends React.Component {
     }
 
     handleMenuClick = (categoryId) => {
-        this.props.history.push(`/products/${categoryId}`)
         if (this.state.id !== categoryId) {
-            const categorizedData = data.filter(value => {
-                if (value.category === categoryId) {
-                    return true
-                }
-                return false
+            let categorizedData
+            this.props.history.push(`/products/${categoryId}`)
+            categorizedData = this.state.categoryData.filter(value => {
+                return value.category === categoryId
             })
-            this.setState({ categoryData: categorizedData, id: categoryId })
+            this.setState({ products: categorizedData, id: categoryId })
         } else {
-            this.setState({ categoryData: data, id: "" })
+            this.setState({ products: this.state.categoryData, id: '' })
         }
     }
     processMenu = (name, id) => {
@@ -82,7 +107,7 @@ class Products extends React.Component {
     processMenuList = () => {
         return <div className="category-list">
             {
-                categories.map(category => {
+                this.state.categories.map(category => {
                     return (
                         <div style={{ backgroundColor: category.id === this.state.id ? 'orange' : "" }}>{this.processMenu(category.name, category.id)}</div>
                     )
@@ -90,20 +115,108 @@ class Products extends React.Component {
             }
         </div>
     }
+    handleOnChange = (e) => {
+        this.handleMenuClick(e.target.value)
+    }
+    processMobileMenuList = () => {
+        return <div className="mobile-category-list">
+            {
+                <select
+                    onChange={this.handleOnChange}
+                >
+                    {
+                        this.state.categories.map(category => {
+                            return (
+                                <option key={category.id} value={category.id}>{category.name}</option>
+                            )
+                        })
+                    }
+                </select>
+            }
+        </div>
+    }
+
     processItemGrid = () => {
         return (
-            <div className="item-container">
-                <div className="section">
-                    <div></div>
-                    <div className="menu-list">
-                        {this.processMenuList()}
+            <>
+                <div className="item-container">
+                    <div className="section">
+                        <div></div>
+                        <div className="menu-list">
+                            {this.processMenuList()}
+                        </div>
+                        <div style={{ marginTop: '1%', height: '100%' }}>
+                            {this.processProductLists(this.state.products)}
+                        </div>
+                        <div></div>
                     </div>
-                    <div style={{ marginTop: '1%', height: '100%' }}>
-                        {this.processProductLists(this.state.categoryData)}
-                    </div>
-                    <div></div>
                 </div>
-            </div>
+                <div className="mobile-item-container">
+                    {this.processMobileMenuList()}
+                    <div style={{ marginTop: '1%', height: '100%' }}>
+                        {this.processProductLists(this.state.products)}
+                    </div>
+                </div>
+            </>
+        );
+    }
+    fetchPostApi = (value) => {
+        fetch("http://localhost:5000/addToCart", {
+            method: 'post',
+            body: JSON.stringify({
+                productId: value.id
+            })
+        })
+            .then(res => {
+                if (res.ok) {
+                    res.json().then(msg => {
+                        this.setState({ open: true, message: msg.responseMessage }, () => {
+                            this.props.buynow(value)
+                            setTimeout(() => {
+                                this.setState({ open: false })
+                            }, 3000)
+                        })
+                    })
+                }
+            })
+            .catch(err => console.log(err))
+    }
+    handleBuyNow = (value) => {
+        this.fetchPostApi(value)
+    }
+
+    processMobileProductCard = (value) => {
+        return (
+            <Card key={value.id} style={{ height: '100%', boxShadow: 'none', borderBottom: 'dashed #e7e7ec' }}>
+                <div className="mobile-product-name">
+                    {value.name}
+                </div>
+                <div className="mobile-card-media">
+                    <div>
+                        <CardMedia
+                            style={{ backgroundSize: 'contain', height: '20vh', padding: '1.5vh' }}
+                            image={value.imageURL}
+                            title={value.name}
+                        />
+                    </div>
+                    <div>
+                        <div className="description">
+                            {value.description}
+                        </div>
+                        <CardActions style={{ boxSizing: "border-box", fontSize: '0.7rem' }}>
+                            <Button
+                                onClick={() => this.handleBuyNow(value)}
+                                id="buy-now"
+                                variant="contained"
+                                color="secondary"
+                            >
+                                {`Buy Now @ MRP Rs.${value.price}`}
+                            </Button>
+                        </CardActions>
+                    </div>
+                </div>
+
+            </Card >
         );
     }
 
@@ -126,7 +239,7 @@ class Products extends React.Component {
                         <strong>{`MRP Rs.${value.price}`}</strong>
                     </p>
                     <Button
-                        onClick={() => this.props.buynow(value)}
+                        onClick={() => this.handleBuyNow(value)}
                         id="buy-now"
                         variant="contained"
                         color="secondary"
@@ -141,13 +254,22 @@ class Products extends React.Component {
 
     processProductLists = (data) => {
         return (
-            <div className="product-list">
-                {data.map(value =>
-                    <div className="product" key={value.id}>
-                        {this.processProductCard(value)}
-                    </div>
-                )}
-            </div>
+            <>
+                <div className="product-list">
+                    {data.map(value =>
+                        <div className="product" key={value.id}>
+                            {this.processProductCard(value)}
+                        </div>
+                    )}
+                </div>
+                <div className="mobile-product-list">
+                    {data.map(value =>
+                        <div className="mobile-product" key={value.id}>
+                            {this.processMobileProductCard(value)}
+                        </div>
+                    )}
+                </div>
+            </>
         );
     }
     render() {
@@ -175,7 +297,7 @@ class Products extends React.Component {
                         open={this.state.open}
                     >
                         <Alert onClose={this.handleClose} severity="success">
-                            {resp.responseMessage}
+                            {this.state.message}
                         </Alert>
                     </Snackbar>
                 }
--- src/container/Home/Home.jsx
+++ src/container/Home/Home.jsx
@@ -6,16 +6,38 @@ import './Home.css';
 import { withRouter } from 'react-router-dom';
 import Carousels from './Carousels'
 
-const banners = require('../../server/banners/index.get.json')
+
 const data = require('../../server/categories/index.get.json')
 
 class Home extends React.Component {
+    state = {
+        banners: [],
+        categories: []
+    }
+
+    fetchAPI = (value) => {
+        fetch(`http://localhost:5000/${value}`, {
+            method: 'get'
+        })
+            .then(res => {
+                res.json().then(data => {
+                    this.setState({ [value]: data })
+                })
+            })
+            .catch(err => {
+                console.log(`Cannot find ${value}, Error is: ${err}`)
+            })
+    }
+    componentDidMount() {
+        this.fetchAPI('banners');
+        this.fetchAPI('categories');
+    }
     onBtnClick = (id) => {
         this.props.history.push(`/products/${id}`)
     }
     processCards = () => {
         const length = data.length - 1
-        return data.map((val, index) => {
+        return this.state.categories.map((val, index) => {
             return <Cards
                 key={val.id}
                 cardContentH5={val.name}
@@ -30,15 +52,16 @@ class Home extends React.Component {
     }
 
     processCarousel = () => {
-        return <Carousels banners={banners} />
+        return <Carousels banners={this.state.banners} />
     }
 
     render() {
+        console.log(this.state)
         return (
             <>
                 <Header />
                 <div className="carousel-container">
-                <div></div>
+                    <div></div>
                     <div>
                         {this.processCarousel()}
                     </div>
