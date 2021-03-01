e02d6baad5520c722d1eb75e0a42125699749071
pavan5164 <zpkplay@gmail.com>
2021-03-01 14:33:25 +0530

    clean up

--- src/sw.js
+++
@@ -1,9 +0,0 @@
-// importScripts('https://cdn.moengage.com/webpush/releases/serviceworker_cdn.min.latest.js');
-// importScripts('libs/workbox-sw.prod.v2.1.2.js');
-
-const workbox = new WorkboxSW({
-    skipWaiting: true,
-    clientsClaim: true
-});
-
-workbox.precache([]);
