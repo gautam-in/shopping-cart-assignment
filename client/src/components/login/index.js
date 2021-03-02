8b5f8284c17a3c4a1243e3e9c4d2b195bfe9507e
neetu <neetuchauhan0421@gmail.com>
2021-03-02 15:40:27 +0530

    added post on login and signup ,showing  error message on every empty  fields

--- client/src/components/login/index.js
+++ client/src/components/login/index.js
@@ -1,14 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import TextField from "@material-ui/core/TextField";
 import "./index.scss";
 
+function errorMessage(field, error) {
+  if (!field && error) return true;
+  else return false;
+}
+function postData(email, password) {
+  var axios = require("axios");
+  var config = {
+    method: "post",
+    url: "http://localhost:5000/login",
+    headers: {},
+  };
+
+  axios(config)
+    .then(function (response) {})
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
   const history = useHistory();
   function submit(e) {
+    if (!email || !password) {
+      setError(true);
+      return;
+    }
     e.preventDefault();
+    postData(email, password);
     history.push("home");
   }
+
+  function handler() {
+    if (!email || !password) {
+      setError(true);
+      return;
+    }
+  }
+
   return (
     <div className={"loginContainer"}>
       <div>
@@ -20,18 +55,28 @@ function Login() {
       <div>
         <form onSubmit={submit} className={"form"}>
           <TextField
+            onChange={(e) => setEmail(e.target.value)}
+            helperText={
+              errorMessage(email, error) ? "Please enter Email." : null
+            }
             type={"email"}
             required
             id="standard-basic"
             label="Email"
+            error={errorMessage(email, error)}
           />
           <TextField
+            onChange={(e) => setPassword(e.target.value)}
+            helperText={
+              errorMessage(password, error) ? "Please enter Password." : null
+            }
             type="password"
             required
             id="standard-basic"
             label="Password"
+            error={errorMessage(password, error)}
           />
-          <button class="btn" type="submit">
+          <button onClick={handler} class="btn" type="submit">
             Login
           </button>
         </form>
