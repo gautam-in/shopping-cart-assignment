c7ba920a1e66530846b9fbb17ed31ff3fe9dd9f6
Ankit Sharma <anksharm25@publicisgroupe.net>
2021-03-19 14:25:59 +0530

    added bootstrap and ngx-bootstrap

--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, CarouselModule.forRoot(), HomeRoutingModule],
 })
 export class HomeModule {}
