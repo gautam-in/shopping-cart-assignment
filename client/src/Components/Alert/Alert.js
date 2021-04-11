0128fdd2892af858d7d9a7ebc3eab07d62362525
gsprasanna <erprasanna97@gmail.com>
2021-04-11 18:17:54 +0530

    Alert component

---
+++ client/src/Components/Alert/Alert.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { useState, useEffect } from 'react'
+import { useDispatch } from "react-redux";
+import { alertActions } from "../../_actions";
+
+const Alert = ({ type, children }) => {
+    debugger;
+    const [show, setShow] = useState(true);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const timeId = setTimeout(() => {
+            // After 3 seconds set the show value to false
+            setShow(false);
+            dispatch(alertActions.clear());
+        }, 2000)
+
+        return () => {
+            clearTimeout(timeId)
+        }
+    }, []);
+
+    return (
+        show ?
+            <div className={`alert ${type}`}>
+                {children}
+            </div>
+            :
+            ""
+    )
+}
+
+export default Alert;
