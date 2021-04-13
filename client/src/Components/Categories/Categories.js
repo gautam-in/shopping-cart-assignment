76f05e6a81b5c7ffa65dfba431871658f02789fd
gsprasanna <erprasanna97@gmail.com>
2021-04-13 19:49:23 +0530

    categories list style fix

--- client/src/Components/Categories/Categories.js
+++ client/src/Components/Categories/Categories.js
@@ -12,46 +12,26 @@ const Categories = ({ categoryList }) => {
         return (
           <section key={item.id} className="category-item">
             {
-              item.order % 2 == 0 ? (
-                <>
-                  <article>
-                    <h3 aria-label={"category name is" + item.name}>{item.name}</h3>
-                    <p aria-label={item.description}>{item.description}</p>
-                    <Link to={routes.productById.replace(":id", item.id)} role="button" aria-label={item.name + 'category button'}>
-                      <CustomButton width="auto" >
-                        {item.key}
-                      </CustomButton>
-                    </Link>
-                  </article>
-                  <figure>
-                    <img
-                      src={item.imageUrl}
-                      alt={item.name + "image"}
-                      height="100"
-                      width="150"
-                      aria-label={item.name + "image"}
-                    />
-                  </figure>
-                </>) : (
-                <>
-                  <figure>
-                    <img
-                      src={item.imageUrl}
-                      alt={item.name + "image"}
-                      height="100"
-                      width="150"
-                      aria-label={item.name + "image"}
-                    />
-                  </figure>
-                  <article>
-                    <h3 aria-label={"category name is" + item.name}>{item.name}</h3>
-                    <p aria-label={item.description}>{item.description}</p>
-                    <Link to={routes.productById.replace(":id", item.id)} role="button" aria-label={item.name + 'category button'}>
-                      <CustomButton width="auto" >{item.key}</CustomButton>
-                    </Link>
-                  </article>
-                </>
-              )
+              <>
+                <article>
+                  <h3 aria-label={"category name is" + item.name}>{item.name}</h3>
+                  <p aria-label={item.description}>{item.description}</p>
+                  <Link to={routes.productById.replace(":id", item.id)} role="button" aria-label={item.name + 'category button'}>
+                    <CustomButton width="auto" >
+                      {item.key}
+                    </CustomButton>
+                  </Link>
+                </article>
+                <figure>
+                  <img
+                    src={item.imageUrl}
+                    alt={item.name + "image"}
+                    height="100"
+                    width="150"
+                    aria-label={item.name + "image"}
+                  />
+                </figure>
+              </>
             }
 
           </section>
