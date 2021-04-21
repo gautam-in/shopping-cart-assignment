5bc96490990b448cf78b75e91990d866c932a2cd
Taniya Paul <tanpaul@publicisgroupe.net>
2021-04-21 11:03:38 +0530

    redux test

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,29 @@
-// pages/_app.js
-import React from "react";
-import {Provider} from "react-redux";
-import App from "next/app";
-import makeStore from "../redux_store";
-import 'bootstrap/dist/css/bootstrap.css'
+import App from 'next/app';
+import {Provider} from 'react-redux';
+import React from 'react';
+import withRedux from "next-redux-wrapper";
+import store from '../redux_store';
 
-const store = makeStore({modalOpen: false})
+class MyApp extends App {
 
-function MyApp({ Component, pageProps, store}) {
-  return (
-    <Provider store={store}>
-        <Component {...pageProps} />
-    </Provider>
-  );
+    static async getInitialProps({Component, ctx}) {
+        const pageProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {};
+
+        //Anything returned here can be accessed by the client
+        return {pageProps: pageProps};
+    }
+
+    render() {
+        //pageProps that were returned  from 'getInitialProps' are stored in the props i.e. pageprops
+          const { Component, pageProps, store } = this.props;
+          return (
+            <Component {...pageProps}/>
+        );
+    }
 }
 
-class MyApp2 extends App {
+//makeStore function that returns a new store for every request
+const makeStore = () => store;
 
-  store = makeStore({modalOpen: false})
-  
-  render() {
-      const {Component, pageProps, store} = this.props;
-      return (
-          <Provider store={store}>
-              <Component {...pageProps} />
-          </Provider>
-      );
-  }
-}
-
-export default MyApp;
+//withRedux wrapper that passes the store to the App Component
+export default withRedux(makeStore)(MyApp);
--- redux_store/index.js
+++ redux_store/index.js
@@ -1,15 +1,6 @@
-import {createStore} from "redux";
-import rootReducer from "./reducer";
+import {createStore} from 'redux';
+import rootReducer from './reducers/rootReducer';
 
-/**
-* @param {object} initialState
-* @param {boolean} options.isServer indicates whether it is a server side or client side
-* @param {Request} options.req NodeJS Request object (not set when client applies initialState from server)
-* @param {Request} options.res NodeJS Request object (not set when client applies initialState from server)
-* @param {boolean} options.debug User-defined debug mode param
-* @param {string} options.storeKey This key will be used to preserve store in global namespace for safe HMR 
-*/
-const makeStore = (initialState, options) => {
-    return createStore(rootReducer, initialState);
-};
-export default makeStore;
+const store = createStore(rootReducer);
+
+export default store;
