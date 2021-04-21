e0ea0f5f7b988bf22a6c06ac10852fe53b355672
gsprasanna <erprasanna97@gmail.com>
2021-04-21 10:03:39 +0530

    fixed the import

--- client/src/Components/Login/_test_/Login.test.js
+++ client/src/Components/Login/_test_/Login.test.js
@@ -1,9 +1,8 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import '@testing-library/jest-dom';
-import { Login } from "../Login";
+import Login from "../Login";
 import configureStore from 'redux-mock-store';
-import { act } from 'react-dom/test-utils';
 import { Provider } from "react-redux";
 
 const mockStore = configureStore([]);
