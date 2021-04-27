198a909740b49c91df4b9b2b9aee954a040f5131
Gowthami11 <44780784+Gowthami11@users.noreply.github.com>
2021-04-27 17:05:08 +0530

    webpack setup

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import React ,{useEffect}from 'react'
 import axios from 'axios'
 import "./App.css"
 export default function App() {
+    const options={
+        mode: "cors",
+        cache: "default",
+        method: "GET",
+        credentials: "same-origin",
+      };
     useEffect(()=>{
         //axios.get('http://localhost:5000/server/addToCart/index.post.json')
-        axios.get('http://localhost:5000/addToCart/index.post.json')
+        fetch('http://localhost:5000/addToCart/index.post.json',options)
     },[])
     return (
         <div className="test">
