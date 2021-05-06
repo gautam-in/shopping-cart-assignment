9226ae13947f030026870a04bc034549ebee0ba0
Neha Sharma <nehsharm18@publicisgroupe.net>
2021-05-06 18:37:15 +0530

    remove unused js

--- bazaar/src/app/app.module.ts
+++ bazaar/src/app/app.module.ts
@@ -7,8 +7,6 @@ import { HomeComponent } from '../app/Component/home/home.component';
 import { HeaderComponent } from './Component/header/header.component';
 import { FooterComponent } from './Component/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
-import {IvyCarouselModule} from 'angular-responsive-carousel';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LoginComponent } from './Component/login/login.component';
 import { RegistrationComponent } from './Component/registration/registration.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -31,8 +29,6 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    IvyCarouselModule,
-    NgbModule,
     ReactiveFormsModule,
     FontAwesomeModule,
     FormsModule
