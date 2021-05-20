64890d6519271c714d1a567508ce268590f51cbd
Naveen Dharni <naveen.dharni@gmail.com>
2021-05-20 23:52:20 +0530

    route: setup routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "pages/Login";
+import Register from "pages/Register";
+import NotFound from "components/Shared/NotFound";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={Login} />
+        <Route exact path="/" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <Route path="*" exact={true} component={NotFound} />
       </Switch>
     </Router>
   );
