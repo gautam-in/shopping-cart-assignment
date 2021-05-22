35efe8af25d6489e85fb64a4eca8c7fbc7ca282b
Ashish Kumar Singh <39079802+ashishya11@users.noreply.github.com>
2021-05-23 02:02:55 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,97 @@
-const path = require("path");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 
-module.exports = {
-  entry: "./src/app.js",
+const ASSET_PATH = process.env.ASSET_PATH || '';
 
-  output: {
-    path: path.resolve("dist"),
-    filename: "bundled.js",
-  },
+module.exports = (env, mode) => {
 
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
+    return {
+        mode: mode,
+
+        entry: './src/app.js',
+
+        output: {
+            filename: '[name].[contenthash].js',
+            publicPath: ASSET_PATH,
+            path: path.resolve(__dirname, './dist'),
+            clean: true,  // Cleaning up the /dist folder
         },
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
+
+        devtool: 'inline-source-map',  // Using source maps
+
+        // Using webpack-dev-server
+        devServer: {
+            contentBase: './public',
+            hot: true,
+            compress: true,
+            port: 3033
+        },
+
+        module: {
+            rules: [
+                {
+                    test: /\.m?js$/,
+                    exclude: /(node_modules)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ["@babel/preset-env", "@babel/preset-react"],
+                            plugins: ['@babel/plugin-proposal-object-rest-spread']
+                        }
+                    }
+                },
+                {
+                    test: /\.scss$/i,
+                    use: [
+                        process.env.NODE_ENV !== 'production'
+                            ? 'style-loader'
+                            : MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        "sass-loader"
+                    ],
+                },
+                {
+                    test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                    type: 'asset/resource',
+                },
+                {
+                    test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                    type: 'asset/resource',
+                },
+            ],
+        },
+        plugins: [
+            new CopyPlugin({
+                patterns: [
+                    "static",
+                    {
+                        from: __dirname + '/static/**/*',
+                        to: __dirname + '/public',
+                    },
+                    {
+                        from: __dirname + '/static/**/*',
+                        to: __dirname + '/dist',
+                    }
+                ],
+                options: {
+                    concurrency: 100,
+                },
+            }),
+            new HtmlWebpackPlugin({
+                template: 'public/index.html'
+            }),
         ],
-      },
-      {
-        test: /\.(eot|otf|ttf|woff|woff2)$/,
-        loader: require.resolve("file-loader"),
-        options: {
-          name: "static/images/[name].[ext]",
+        optimization: {
+            removeAvailableModules: false,
+            removeEmptyChunks: false,
+            splitChunks: false,
         },
-      },
-      //Allows use search url of images
-      {
-        test: /\.(png|jpg|gif|svg)$/i,
-        use: {
-          loader: "url-loader",
-          options: {
-            limit: 8192,
-            name: "static/images/[name].[ext]",
-          },
-        },
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./public/index.html",
-      filename: "./index.html",
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true,
-        removeAttributeQuotes: true,
-      },
-    }),
-  ],
+        performance: {
+            hints: false,
+            maxEntrypointSize: 512000,
+            maxAssetSize: 512000
+        }
+    }
 };
