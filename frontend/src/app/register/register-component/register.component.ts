d08e16edc6969412f03cdcdfd30013184e9569c0
Neha shetty <neha.n@publicissapient.com>
2021-05-24 22:35:41 +0530

    registration validations added

--- frontend/src/app/register/register-component/register.component.ts
+++ frontend/src/app/register/register-component/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,17 +8,48 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup = new FormGroup({});
   constructor(private fb: FormBuilder, private router: Router) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.createRegisterForm();
+  }
 
-  registerForm = this.fb.group({
-    fName: [''],
-    lName: [''],
-    email: [''],
-    password: [''],
-    confirmPassword: [''],
-  });
+  createRegisterForm() {
+    this.registerForm = this.fb.group(
+      {
+        fName: ['', Validators.required],
+        lName: ['', Validators.required],
+        email: ['', Validators.required],
+        password: ['', Validators.required],
+        confirmPassword: ['', Validators.required],
+      },
+      {
+        validator: this.mustMatch('password', 'confirmPassword'),
+      }
+    );
+  }
+
+  mustMatch(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+      const control = formGroup.controls[controlName];
+      const matchingControl = formGroup.controls[matchingControlName];
+
+      if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+        return;
+      }
+
+      if (control.value !== matchingControl.value) {
+        matchingControl.setErrors({ mustMatch: true });
+      } else {
+        matchingControl.setErrors(null);
+      }
+    };
+  }
+
+  get formControl() {
+    return this.registerForm.controls;
+  }
 
   onSubmit(): void {
     console.log(this.registerForm.value);
