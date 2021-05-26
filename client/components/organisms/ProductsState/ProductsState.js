70d7bd756fa80c070beb27199a5e6e8915e124d3
Abhishek <abhishekghosh018@gmail.com>
2021-05-26 15:21:48 +0530

    minor change

--- client/components/organisms/ProductsState/ProductsState.js
+++ client/components/organisms/ProductsState/ProductsState.js
@@ -5,19 +5,6 @@ function ProductsState({ children, dataList, status: { loading, error } }) {
   if (error) return <div>Error fetching data.</div>;
   if (dataList.length === 0) return <div>No data present.</div>;
   return children;
-  /* return (
-    <>
-      {loading ? (
-        <div>Loading...</div>
-      ) : error ? (
-        <div>Error fetching data.</div>
-      ) : dataList.length === 0 ? (
-        <div>No data present.</div>
-      ) : (
-        children
-      )}
-    </>
-  ); */
 }
 
 export default React.memo(ProductsState);
