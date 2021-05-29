16425e5db3e4c7b269b2f330e69bbfe4a0958c41
Neha shetty <neha.n@publicissapient.com>
2021-05-29 10:56:52 +0530

    all products listed

--- frontend/src/app/product-listing/products-component/product-listing.component.ts
+++ frontend/src/app/product-listing/products-component/product-listing.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { FetchProductsService } from 'src/app/services/fetch-products.service';
 
 @Component({
   selector: 'app-product-listing',
   templateUrl: './product-listing.component.html',
-  styleUrls: ['./product-listing.component.scss']
+  styleUrls: ['./product-listing.component.scss'],
 })
 export class ProductListingComponent implements OnInit {
+  allCategories: any[] = [];
+  allProducts: any[] = [];
+  selectedCategory: any[] = [];
 
-  constructor() { }
+  selected: boolean = false;
+  constructor(private fetchProducts: FetchProductsService) {}
 
   ngOnInit(): void {
+    this.getAllCategories();
+    this.getAllProducts();
   }
 
+  getAllCategories(): void {
+    this.fetchProducts.getCategories().subscribe((res: any) => {
+      this.allCategories = res;
+      console.log(this.allCategories);
+    });
+  }
+
+  getAllProducts(): void {
+    this.fetchProducts.getProducts().subscribe((res: any) => {
+      this.allProducts = res;
+      console.log(this.allProducts);
+    });
+  }
+
+  showProducts(categoryId:any): void {
+    console.log(categoryId);
+    // this.selected = true;
+
+    // if (this.selected === true) {
+    //   this.selectedCategory = this.allProducts.filter(
+    //     (product) => product.category === categoryId
+    //   );
+    // }
+  }
 }
