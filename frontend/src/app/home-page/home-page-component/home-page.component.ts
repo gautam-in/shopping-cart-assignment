d368492a2e99942b2f0cdd421f74f46b0bcf286c
Neha shetty <neha.n@publicissapient.com>
2021-05-29 10:51:21 +0530

    ngx -carousel & categories set

--- frontend/src/app/home-page/home-page-component/home-page.component.ts
+++ frontend/src/app/home-page/home-page-component/home-page.component.ts
@@ -5,14 +5,47 @@ import { FetchProductsService } from 'src/app/services/fetch-products.service';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: [
-    './home-page.component.scss',
-    './../../../styles/bootstrap.min.css',
-  ],
+  styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent implements OnInit {
-  allBanners: any = [];
-  allCategories: any = [];
+  allBanners: any[] = [];
+  allCategories: any[] = [];
+
+  customOptions: any = {
+    loop: true,
+    autoplay: true,
+
+    dots: true,
+    navSpeed: 500,
+    // center: true,
+    items: 1,
+
+    margin: 4,
+    autoWidth: true,
+
+    // responsiveClass: true,
+    // responsive: {
+    //   0: {
+    //     items: 1,
+    //     nav: true,
+    //   },
+    //   600: {
+    //     items: 1,
+    //     nav: true,
+    //   },
+    //   1000: {
+    //     items: 2,
+    //     nav: true,
+    //     loop: false,
+    //   },
+    //   1500: {
+    //     items: 3,
+    //     nav: true,
+    //     loop: false,
+    //   },
+    // },
+    // nav: true,
+  };
 
   constructor(
     private fetchBanners: FetchProductsService,
@@ -39,6 +72,6 @@ export class HomePageComponent implements OnInit {
   }
 
   navigateToExplore(): void {
-    this.router.navigate(['/products'])
+    this.router.navigate(['/products']);
   }
 }
--- frontend/src/app/home-page/home-page.module.ts
+++ frontend/src/app/home-page/home-page.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { HomePageRoutingModule } from './home-page-routing.module';
 import { HomePageComponent } from './home-page-component/home-page.component';
 import { SharedModule } from '../shared/shared.module';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 
 @NgModule({
   declarations: [HomePageComponent],
-  imports: [CommonModule, HomePageRoutingModule, SharedModule],
+  imports: [CommonModule, HomePageRoutingModule, SharedModule, CarouselModule],
 })
 export class HomePageModule {}
