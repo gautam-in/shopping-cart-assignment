5585d837c379fb1c52be73879e0a2f8a3da7eb38
Neha shetty <neha.n@publicissapient.com>
2021-05-29 16:06:05 +0530

    Footer added

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderModule } from './header/header.module';
+import { FooterModule } from './footer/footer.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, HeaderModule],
-  exports: [HeaderModule],
+  imports: [CommonModule, HeaderModule, FooterModule],
+  exports: [HeaderModule, FooterModule],
 })
 export class SharedModule {}
