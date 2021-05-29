712c5443a2136196e45c0daa89c39a4884caeb96
Neha shetty <neha.n@publicissapient.com>
2021-05-29 10:56:13 +0530

    angular material modules imported

--- frontend/src/app/product-listing/product-listing.module.ts
+++ frontend/src/app/product-listing/product-listing.module.ts
@@ -4,9 +4,21 @@ import { CommonModule } from '@angular/common';
 import { ProductListingRoutingModule } from './product-listing-routing.module';
 import { ProductListingComponent } from './products-component/product-listing.component';
 import { SharedModule } from '../shared/shared.module';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [ProductListingComponent],
-  imports: [CommonModule, ProductListingRoutingModule, SharedModule],
+  imports: [
+    CommonModule,
+    ProductListingRoutingModule,
+    SharedModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+    MatButtonModule,
+  ],
 })
 export class ProductListingModule {}
