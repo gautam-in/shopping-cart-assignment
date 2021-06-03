e1da3205d6d9c1593774eb1605b91268f17470fd
Neha shetty <neha.n@publicissapient.com>
2021-06-03 22:52:39 +0530

    created new component for category

---
+++ frontend/src/app/shared/category/category.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CategoryComponent } from './category.component';
+
+@NgModule({
+  declarations: [CategoryComponent],
+  imports: [CommonModule],
+  exports: [CategoryComponent],
+})
+export class CategoryModule {}
