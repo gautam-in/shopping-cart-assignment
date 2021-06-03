1dd4128b3ae3d616bf3d152c4bae062363c43aa3
Neha shetty <neha.n@publicissapient.com>
2021-06-03 19:04:13 +0530

    removed type annotation as per tslint error

--- frontend/src/app/login/login-component/login.component.ts
+++ frontend/src/app/login/login-component/login.component.ts
@@ -1,10 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  Validators,
-} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
@@ -15,7 +10,7 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup = new FormGroup({});
-  submitted: boolean = false;
+  submitted = false;
 
   constructor(
     private router: Router,
