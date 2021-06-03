f87ff5cc40a3064160868f06740b0c830a74f3eb
Neha shetty <neha.n@publicissapient.com>
2021-06-04 00:04:10 +0530

    category module & component added

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderModule } from './header/header.module';
 import { FooterModule } from './footer/footer.module';
+import { CategoryModule } from './category/category.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, HeaderModule, FooterModule],
-  exports: [HeaderModule, FooterModule],
+  imports: [CommonModule, HeaderModule, FooterModule, CategoryModule],
+  exports: [HeaderModule, FooterModule, CategoryModule],
 })
 export class SharedModule {}
