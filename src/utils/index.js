b1082d1f542ba30f0cbbae05818a978a68dcca91
ashishya11 <ashishsingh.ashishya@gmail.com>
2021-06-10 12:50:13 +0530

    code is fixed.

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,7 @@
 import FetchData from "./fetch-data";
 import LocalStorage from "./local-storage";
 import SessionStorage from "./session-storage";
-import pubsub from "./pubsub";
+// import pubsub from "./pubsub";
 import useDevice from "./customHooks/useDevices";
 
-export { FetchData, useDevice, LocalStorage, SessionStorage, pubsub };
+export { FetchData, useDevice, LocalStorage, SessionStorage };
