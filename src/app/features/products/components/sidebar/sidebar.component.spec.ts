dc3dc473b6b2c18352f14b2948800970ae99bea9
sansingh45 <sanjeet.singh1@publicissapient.com>
2021-06-10 18:55:38 +0530

    auth reducer unit testing.

--- src/app/features/products/components/sidebar/sidebar.component.spec.ts
+++ src/app/features/products/components/sidebar/sidebar.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { SidebarComponent } from './sidebar.component';
 
@@ -8,7 +9,8 @@ describe('SidebarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SidebarComponent ]
+      declarations: [ SidebarComponent ],
+      imports: [RouterTestingModule]
     })
     .compileComponents();
   });
