6e08bd699c0da237ae2cceca1286d95e2e52f476
Shruti <shrutiagrawal915@gmail.com>
2021-06-21 12:13:42 +0530

    Carousel

--- react/Components/carousel.js
+++ react/Components/carousel.js
@@ -2,29 +2,42 @@ import React, { useState } from 'react';
 import '../Styles/carousel.css';
 
 export default function ({ banners }) {
-  let [banner, setBanner] = useState(banners[0]);
-
+  let [banner, setBanner] = useState(0);
+  let changePrev = () => {
+    if (banner == 0) return;
+    setBanner(banner - 1);
+  };
+  let changeNext = () => {
+    if (banner + 1 == banners.length) return;
+    setBanner(banner + 1);
+  };
   return (
     <div className="banners">
       <div className="carousel">
-        <div className="prev">Prev</div>
+        <div className="prev" onClick={changePrev}>
+          Prev
+        </div>
         <img
           className="banner-image"
-          src={banner.bannerImageUrl}
-          alt={banner.bannerImageAlt}
+          src={banners[banner].bannerImageUrl}
+          alt={banners[banner].bannerImageAlt}
         />
         <div className="banner-dots">
-          {banners.map((_) => (
+          {banners.map((_, i) => (
             <div
               key={_.id}
-              className={_.id == banner.id ? 'banner-dot active' : 'banner-dot'}
+              className={
+                _.id == banners[banner].id ? 'banner-dot active' : 'banner-dot'
+              }
               onClick={() => {
-                setBanner(_);
+                setBanner(i);
               }}
             />
           ))}
         </div>
-        <div className="next">Next</div>
+        <div className="next" onClick={changeNext}>
+          Next
+        </div>
       </div>
     </div>
   );
