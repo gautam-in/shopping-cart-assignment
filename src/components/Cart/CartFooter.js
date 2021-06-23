4bff33fac7b6480c0e4395b8d79bfc5b9aed1ba8
Naveen Dharni <naveen.dharni@gmail.com>
2021-06-23 17:00:02 +0530

    chore: added buttons for acessibility

--- src/components/Cart/CartFooter.js
+++ src/components/Cart/CartFooter.js
@@ -4,7 +4,12 @@ const CartFooter = ({ closeCart, totalprice }) => {
   return (
     <Container>
       <div className="promo">Promo code can be applied on payment page</div>
-      <div className="proceed-btn" onClick={closeCart}>
+      <div
+        role="button"
+        tabIndex="0"
+        className="proceed-btn"
+        onClick={closeCart}
+      >
         <div>Proceed to Checkout</div>
         <div>Rs.{totalprice} &gt;</div>
       </div>
