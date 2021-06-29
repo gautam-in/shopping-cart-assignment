fa507a5bf73517acb2a78de77a497cab73a49b16
Malay <malayvarma@gmail.com>
2021-06-29 13:13:01 +0530

    test

--- client/components/Layout/Header.js
+++ client/components/Layout/Header.js
@@ -3,13 +3,14 @@ import Nav from './Nav'
 import styles from '../../styles/header.module.scss'
 import Link from 'next/link'
 
+
 const Header = () => {
     return (
         <header className={styles.headerContainer}>
             <div className={styles.header}>
                 <div className={styles.logo}>
                     <Link href="/">
-                      <img src="/static/images/logo.png" alt='img' />
+                        <img src="/static/images/logo.png" alt='img' />
                     </Link>
                 </div>
                 <Nav />
