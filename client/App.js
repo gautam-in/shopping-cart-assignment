34b4c35d7c51dd9c05fa83782e34ac287fa63531
Anukritik007 <anukritik77@gmail.com>
2021-07-07 14:01:32 +0530

    webpack, code splitting, lazy load

--- client/App.js
+++ client/App.js
@@ -1,16 +1,16 @@
 import "./App.scss";
-import React, { useState } from "react";
+import React, { useState, lazy, Suspense } from "react";
 import { Provider } from "react-redux";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./containers/Home/Home";
 import Footer from "./components/Footer/Footer";
-import Products from "./containers/Products/Products";
 import Register from "./containers/Auth/Register";
 import SignIn from "./containers/Auth/SignIn";
-import Cart from "./containers/Cart/Cart";
 import AuthContext from "./AuthContext";
 import store from "./redux/store";
+const Products = lazy(() => import("./containers/Products/Products"));
+const Cart = lazy(() => import("./containers/Cart/Cart"));
 
 const App = () => {
   const [userAuthentication, setUserAuthentication] = useState(
@@ -31,14 +31,16 @@ const App = () => {
         <BrowserRouter>
           <Header />
           <main>
-            <Switch>
-              <Route exact path="/" render={() => <Redirect to="/home" />} />
-              <Route path="/login" component={SignIn} />
-              <Route path="/sign-up" component={Register} />
-              <Route path="/home" component={Home} />
-              <Route path="/products" component={Products} />
-              <Route path="/cart" component={Cart} />
-            </Switch>
+            <Suspense fallback={<small>Loading products...</small>}>
+              <Switch>
+                <Route exact path="/" render={() => <Redirect to="/home" />} />
+                <Route path="/login" component={SignIn} />
+                <Route path="/sign-up" component={Register} />
+                <Route path="/home" component={Home} />
+                <Route path="/products" component={Products} />
+                <Route path="/cart" component={Cart} />
+              </Switch>
+            </Suspense>
           </main>
           <Footer />
         </BrowserRouter>
