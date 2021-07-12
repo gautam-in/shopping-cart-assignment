38120e22716f0decde141c28ed62030812ba387a
Aarshi-Sapient <kumaraarshi.anand@publicissapient.com>
2021-07-12 23:42:27 +0530

    Changes to aws

--- expressServer/utils/aws.js
+++ expressServer/utils/aws.js
@@ -2,11 +2,13 @@ const AWS = require('aws-sdk');
 
 AWS.config.loadFromPath("./utils/aws_config.json");
 
-const s3 = new AWS.S3()
+const s3 = new AWS.S3({
+    signatureVersion: "v4"
+})
 
 const getPreSignedUrl = ( fileKey ) => {
     return s3.getSignedUrl("getObject", {
-        Bucket: "sabkabazaar",
+        Bucket: "sabkabazaar0",
         Key: fileKey,
         Expires: 10 * 60,
     });
