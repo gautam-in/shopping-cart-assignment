e01c6e9412c879391760663ae94f102ecb31cd69
Shubham Agarwal <shubhamrocks0709@gmail.com>
2021-09-05 03:56:40 +0530

    home page completed with desktop/tablet/mobile responsiveness

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 // Here, you write different options and tell Webpack what to do
 module.exports = {
   // Path to your entry point. From this file Webpack will begin its work
-  entry: './src/javascript/index.js',
+  entry: './src/js/index.js',
 
   // Path and filename of your result bundle.
   // Webpack will bundle all JavaScript into this file
