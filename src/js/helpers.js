6b51e63f68b68976abe3101a941d3e41ae940e95
Aashiish-Pandey <ash006pandey@gmail.com>
2021-09-08 23:24:05 +0530

    assignment code

---
+++ src/js/helpers.js
@@ -0,0 +1,19 @@
+const createElementHelper = (type, classList, innerText, innerHTML) => {
+  const customElement = document.createElement(type);
+  if (classList) customElement.classList = classList;
+  if (innerText) customElement.innerText = innerText;
+  if (innerHTML) customElement.innerHTML = innerHTML;
+
+  return customElement;
+};
+
+const makeRequest = async (url) => {
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw Error('Something went wrong');
+  }
+};
