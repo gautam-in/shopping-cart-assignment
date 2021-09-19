ccab8c464923ce92fb24a45869be554958270e27
Sourav Saha <souravsaha101@gmail.com>
2021-09-20 02:42:26 +0530

    folder structure chnage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
 import "./App.scss";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Landing from "./components/pages/Landing/Landing";
-import Header from "./components/common/Header/Header";
-import Footer from "./components/common/Footer/Footer";
-
+import Landing from "./pages/Landing/Landing";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 export default function App() {
   return (
---
+++ src/components/HeroCarousel/HeroCarousel.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./HeroCarousel.scss"
+
+export default function HeroCarousel() {
+  return (
+    <>
+    <h1>HeroCarousel</h1>
+    </>
+  );
+}
