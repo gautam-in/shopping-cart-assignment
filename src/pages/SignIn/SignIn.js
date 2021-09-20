03fbe20acf0f5888d1905fbf5b88b33c9cd1a769
Sourav Saha <souravsaha101@gmail.com>
2021-09-21 03:49:47 +0530

    Signin Page  done

--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React from "react";
+import "./SignIn.scss";
 
 export default function SignIn() {
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
-    <>
-    <h1>SignIn Page</h1>
-    </>
+    <section className="signin">
+      <div className="signin-text">
+        <h1>Login</h1>
+        <p>Get access to your orders, wishlist and recommedations</p>
+      </div>
+      <div className="signin-action">
+        <form onSubmit={onFormSubmit} className="signin-action-form">
+          <input type="email" placeholder="Email" required />
+          <input type="password" placeholder="Password" required />
+          <button>Login</button>
+        </form>
+      </div>
+    </section>
   );
 }
