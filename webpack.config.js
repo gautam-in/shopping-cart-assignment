51252576513bef8000253b835b0d1f81ad39f480
Navdeep singh <navsingh23@WKMZT0526580.global.publicisgroupe.net>
2021-09-20 10:33:37 +0530

    Webpack Setup: Done

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const app = path.resolve(__dirname, "src", "js", "index.js");
+
+module.exports = {
+  entry: "./src/js/index.js",
+  mode: "production",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        exclude: /node_modules/,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+};
