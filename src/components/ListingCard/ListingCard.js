3f721e0f3bf693d1730daffea78c55c999e1d665
Sourav Saha <souravsaha101@gmail.com>
2021-09-22 01:12:05 +0530

    product list ui

--- src/components/ListingCard/ListingCard.js
+++ src/components/ListingCard/ListingCard.js
@@ -18,7 +18,32 @@ export default function ListingCard({ data }) {
   return (
     <div className="listing-product">
       {cardData && cardData.length ? (
-        cardData.map((el) => <h1> {el.name}</h1>)
+        cardData.map((el) => (
+          <div key={el.id} className="listing-product-element">
+            <div className="listing-product-element-title">{el.name}</div>
+            <div className="listing-product-element-content">
+              <img
+                src={el.imageURL}
+                alt={el.name}
+                className="listing-product-element-image"
+                height="120"
+                width="100"
+              />
+              <div className="listing-product-element-desc">
+                {el.description}
+              </div>
+            </div>
+            <div className="listing-product-element-footer">
+              <div className="price-lg">MRP Rs.{el.price}</div>
+              <button className="listing-product-element-button button-lg">
+                Buy Now
+              </button>
+              <button className="listing-product-element-button button-sm">
+                Buy Now @ Rs.{el.price}
+              </button>
+            </div>
+          </div>
+        ))
       ) : (
         <h1> No data</h1>
       )}
--- src/pages/Listing/Listing.js
+++ src/pages/Listing/Listing.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Listing.scss";
 import CategoryList from "../../components/CategoryList/CategoryList";
 import CategoryDropdown from "../../components/CategoryDropDown/CategoryDropdown";
@@ -7,6 +7,7 @@ import ListingList from "../../components/ListingList/ListingList";
 export default function Listing() {
   let categoryData, categoryLoading, categoryError;
   const storageData = localStorage.getItem("category-list");
+  const [width, setWidth] = useState(window.innerWidth);
 
   if (storageData && JSON.parse(storageData)?.length) {
     categoryData = JSON.parse(storageData);
@@ -17,11 +18,28 @@ export default function Listing() {
     localStorage.setItem("category-list", JSON.stringify(categoryData));
   }
 
+  useEffect(() => {
+    function handleResize() {
+      setWidth(window.innerWidth);
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <section>
-      {/* <CategoryList data={categoryData}/> */}
-      <CategoryDropdown data={categoryData} />
-      <ListingList />
+      {width > 768 ? (
+        <div className="list">
+          <CategoryList data={categoryData} />
+          <ListingList />
+        </div>
+      ) : (
+        <>
+          <CategoryDropdown data={categoryData} />
+          <ListingList />
+        </>
+      )}
     </section>
   );
 }
