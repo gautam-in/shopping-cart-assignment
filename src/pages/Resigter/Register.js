31a839125843d1377f126160b01ef10fc5efcda9
Sourav Saha <souravsaha101@gmail.com>
2021-09-21 12:27:42 +0530

    Signin and Resister Page functanity added

--- src/pages/Resigter/Register.js
+++ src/pages/Resigter/Register.js
@@ -1,9 +1,107 @@
-import React from 'react';
+import React, { useState } from "react";
+import "./Register.scss";
+import { useHistory } from "react-router-dom";
+
+const initialState = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  confirmPwd: "",
+};
 
 export default function Register() {
+  const [formData, setFormData] = useState(initialState);
+  const [userMessage, setUserMessage] = useState("");
+  const [isSuccess, setIsSuccess] = useState(false);
+
+  const history = useHistory();
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    const regPassword = /^(?!.* )(?=.*\d)(?=.*[A-Z]).{6,18}$/;
+    if (formData.firstName && formData.email && formData.password) {
+      if (formData.password !== formData.confirmPwd) {
+        setUserMessage("Passwords Don't Match");
+        return;
+      }
+      if (!regPassword.test(formData.password)) {
+        setUserMessage(
+          "Passwords Must Contain 6 Characters, A Number And A Alphabet"
+        );
+        return;
+      }
+      let userData = {
+        email: formData.email,
+        password: formData.password,
+      };
+      setUserMessage("User Registered Successfully");
+      setIsSuccess(true);
+      localStorage.setItem("user-details", JSON.stringify(userData));
+      setTimeout(() => history.push("/signin"), 1500);
+    } else {
+      setUserMessage("Fill All The Fields");
+      return;
+    }
+  };
+  const formChangeHandler = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
   return (
-    <>
-    <h1>Register Page</h1>
-    </>
+    <section className="signin">
+      <div className="signin-text">
+        <h1>Signup</h1>
+        <p>We do not share your personal details with anyone</p>
+      </div>
+      <div className="signin-action">
+        <form onSubmit={onFormSubmit} className="signin-action-form">
+          <input
+            type="text"
+            id="firstName"
+            placeholder="First Name"
+            required
+            value={formData.firstName}
+            onChange={formChangeHandler}
+          />
+          <input
+            type="text"
+            id="lastName"
+            placeholder="Last Name"
+            required
+            value={formData.lastName}
+            onChange={formChangeHandler}
+          />
+          <input
+            type="email"
+            id="email"
+            placeholder="Email"
+            required
+            value={formData.email}
+            onChange={formChangeHandler}
+          />
+          <input
+            type="password"
+            id="password"
+            placeholder="Password"
+            required
+            value={formData.password}
+            onChange={formChangeHandler}
+          />
+          <input
+            type="password"
+            id="confirmPwd"
+            placeholder="Confirm Password"
+            required
+            value={formData.confirmPwd}
+            onChange={formChangeHandler}
+          />
+          {userMessage && (
+            <span className={isSuccess ? "msg success-msg" : "msg error-msg"}>
+              {userMessage}
+            </span>
+          )}
+          <button>Signup</button>
+        </form>
+      </div>
+    </section>
   );
 }
--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SignIn.scss";
+import { useHistory } from "react-router-dom";
+
+const initialState = {
+  email: "",
+  password: "",
+};
 
 export default function SignIn() {
+  const [formData, setFormData] = useState(initialState);
+  const [userMessage, setUserMessage] = useState("");
+  const [isSuccess, setIsSuccess] = useState(false);
+
+  const history = useHistory();
   const onFormSubmit = (e) => {
     e.preventDefault();
+    if (formData.email && formData.password) {
+      let userData = JSON.parse(localStorage.getItem("user-details"));
+      if (!userData) {
+        setUserMessage("No User Present, Register First");
+        return;
+      }
+      debugger
+      if (
+        userData.email !== formData.email &&
+        userData.password !== formData.password
+      ) {
+        setUserMessage("Invalid Credentials");
+        return;
+      }
+      setUserMessage("Login Successfully");
+      setIsSuccess(true);
+      localStorage.setItem("user-status", "loggedIn");
+      setTimeout(() => history.push("/"), 1500);
+    } else {
+      setUserMessage("Fill All The Fields");
+      return;
+    }
+  };
+  const formChangeHandler = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
   };
   return (
     <section className="signin">
@@ -13,9 +49,28 @@ export default function SignIn() {
       </div>
       <div className="signin-action">
         <form onSubmit={onFormSubmit} className="signin-action-form">
-          <input type="email" placeholder="Email" required />
-          <input type="password" placeholder="Password" required />
-          <button>Login</button>
+          <input
+            type="email"
+            id="email"
+            placeholder="Email"
+            required
+            value={formData.email}
+            onChange={formChangeHandler}
+          />
+          <input
+            type="password"
+            id="password"
+            placeholder="Password"
+            required
+            value={formData.password}
+            onChange={formChangeHandler}
+          />
+          {userMessage && (
+            <span className={isSuccess ? "msg success-msg" : "msg error-msg"}>
+              {userMessage}
+            </span>
+          )}
+          <button>Signup</button>
         </form>
       </div>
     </section>
