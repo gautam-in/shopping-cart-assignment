165406fb451bfa0e9e1ab554975ae8904ccdf21e
Gokaran Jindal <kjindal0802@gmail.com>
2021-11-23 11:53:11 +0530

    Initial React Commit

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="app-footer">
+      <b>Copyright © 2011-2020 Sabka Bazaar Grocery Supplies PVT LTD</b>
+    </footer>
+  );
+}
+
+export default Footer;
