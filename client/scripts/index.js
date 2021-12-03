98b3d4eb3d726b5683b49a368998ab8121ebad27
Jemesh Joseph <jemesh.joseph@publicissapient.com>
2021-12-03 13:10:38 +0530

    header UI fixes , footer UI completed, login component started

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -5,11 +5,13 @@ import Handlebars from "handlebars";
 import template from "../templates/layout.handlebars";
 
 (function () {
-  //   var source = document.getElementById("entry-template").innerHTML;
-  //   var template = Handlebars.compile(source);
   var context = { title: "My New Post 12345", body: "This is my first post!" };
-  //   var context = {};
   var html = template(context);
+  document.querySelector("#app-root").innerHTML = html; // Call multiple times to show different pages data
 
-  document.querySelector("#app-root").innerHTML = html;
+  // var source = document.getElementById("entry-template").innerHTML;
+  // var template2 = Handlebars.compile(source);
+  // // var context2 = { title: "My New Post 12345", body: "This is my first post!" };
+  // var html2 = template2({});
+  // document.querySelector("#route-content").innerHTML = html2;
 })();
