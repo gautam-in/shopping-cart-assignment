56269b93d589167903328208244d8d4e0292e283
Umar Khan <umakhan@publicisgroupe.net>
2021-12-08 19:43:41 +0530

    Assihnment Done

---
+++ client/pages/components/footer.js
@@ -0,0 +1,5 @@
+function displayFooter() {
+  const footer = document.getElementById("footer");
+  footer.innerHTML += `<div class='footer'>Copyright © 2011-2018 Sabka Bazaar Grocery Supplies Pvt Ltd</div>`;
+}
+displayFooter();
