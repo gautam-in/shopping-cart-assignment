ec0e6f164320a1f2d83413a6b04e30dd91ccc0c8
Akshay Mittal <aksmitta2@WKMZT58CC5A3.global.publicisgroupe.net>
2021-12-22 12:32:31 +0530

    products and home  page completed for desktop version

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -4,44 +4,69 @@ const categoryContainer = document.querySelector(".categories-container");
 const  carouselBlock = document.querySelector(".carousel-inner");
 
 
+
+function exploreCategory(eventData) {
+    const id = eventData.target.getAttribute("data-id");
+    sessionStorage.setItem('categoryId',id);
+    window.location.href = "products.html"
+}
+
  function loadData () {
+     if(sessionStorage.getItem('categoryId')) {
+         sessionStorage.removeItem('categoryId');
+     }
   loadBanners();
   loadCategories();  
 }
 
 async function loadBanners() {
-    const response  = await fetch(API_PATH.bannersUrl);
-    const data = await response.json();
-    const carouselItems  = data.filter(banner => banner.isActive)
-      .map((banner,index) => {
-         return `<div class="${index === 0?'carousel-item active': 'carousel-item'}">
-         <img class="d-block w-100" src="${banner.bannerImageUrl}" alt="${banner.bannerImageAlt}">
-       </div>`
-
-       });
-       carouselBlock.innerHTML = carouselItems.join('');
+    try {
+        const response  = await fetch(API_PATH.bannersUrl);
+        const data = await response.json();
+        const carouselItems  = data.filter(banner => banner.isActive)
+          .map((banner,index) => {
+             return `<div class="${index === 0?'carousel-item active': 'carousel-item'}">
+             <img class="d-block w-100" src="${banner.bannerImageUrl}" alt="${banner.bannerImageAlt}">
+           </div>`
+    
+           });
+           carouselBlock.innerHTML = carouselItems.join('');
+    }catch(error) {
+        console.log("error",error);
+    }
+    
 }
 
 async function loadCategories() {
-    const response  = await fetch(API_PATH.categoriesUrl);
-    const data = await response.json();
+    try {
+        const response  = await fetch(API_PATH.categoriesUrl);
+        const data = await response.json();
 
-    categoryContainer.innerHTML = data.filter(category => category.enabled).map((category,index) => {
-        return `
-           <div class="row category ${index%2 !== 0 ? 'flex-row-reverse': ''}"> 
-            <div class="col-4 img-conatiner">
-                    <img class="img-fluid" src="${category.imageUrl}" alt="${category.name}">
-                </div>
-                <div class=" col-8 category-details">
-                    <h2>${category.name}</h2>
-                    <p>${category.description}}</p>
-                    <button class="btn btnCrimsonPink" type="button"
-                        aria-label="Click here to explore ${category.key}">Explore
-                        ${category.key}</button>
-                </div>
-            </div>    `
-    }).join('');
+        categoryContainer.innerHTML = data.filter(category => category.enabled).map((category,index) => {
+            return `
+            <div class="row category ${index%2 !== 0 ? 'flex-row-reverse': ''}"> 
+                <div class="col-4 img-conatiner">
+                        <img class="img-fluid" src="${category.imageUrl}" alt="${category.name}">
+                    </div>
+                    <div class=" col-8 category-details">
+                        <h2>${category.name}</h2>
+                        <p>${category.description}}</p>
+                        <button class="btn btnCrimsonPink" data-id="${category.id}" type="button"
+                            aria-label="Click here to explore ${category.key}" >Explore
+                            ${category.key}</button>
+                    </div>
+                </div> `
+        }).join('');
+    }catch(error) {
+        console.log("error",error);
+    }
+    
+  const categoryButtons = document.querySelectorAll('.category-details button');
+  
+  categoryButtons.forEach(button => button.addEventListener('click',exploreCategory));
+
 }
+
 
 
 document.addEventListener('DOMContentLoaded',loadData);
--- public/scripts/products.js
+++ public/scripts/products.js
@@ -1,36 +1,142 @@
 import { API_PATH } from './constants.js';
-let categories = [];
-let products = [];
+
 
 const listGourp = document.querySelector(".category-side-nav");
- console.log(listGourp);
-function loadData() {
-    loadCategories();
-    loadProducts();
+let products = [];
+let previousListItem = null;
+
+async function loadData() {
+    try {
+        await loadCategories();
+        await loadProducts();
+    }catch(error) {
+        console.log('error',error);
+    }
+    
 }
 
-
+//function to load categories
 async function loadCategories() {
-    const response  = await fetch(API_PATH.categoriesUrl);
-    categories = await response.json();
-    console.log(categories);
-     categories.filter(category => category.enabled)
-      .forEach(category => {
-           let button = document.createElement('button');
-            button.setAttribute('type','button');
-            button.setAttribute('data-id',category.id);
-            button.className = 'list-group-item list-group-item-action';
-            button.innerText=category.name
-           listGourp.appendChild(button);
-      });
+    try {
+        const response  = await fetch(API_PATH.categoriesUrl);
+        let categories = await response.json();
+        categories.filter(category => category.enabled)
+          .forEach(category => {
+               let button = document.createElement('button');
+                button.setAttribute('type','button');
+                button.setAttribute('data-id',category.id);
+                button.className = 'list-group-item list-group-item-action';
+                button.innerText=category.name
+               listGourp.appendChild(button);
+          });
+    
+          listGourp.addEventListener('click',filterProducts);
+    } catch(error){
+        console.log("error",error);
+    }    
 
 }
 
-
+//function to load products initially
 async function loadProducts() {
-    const response = await fetch(API_PATH.productsUrl); 
-    products = await response.json();
-    console.log(products);
+    let localProducts;
+    try {
+        const response = await fetch(API_PATH.productsUrl); 
+        products = await response.json();
+        
+        const categoryId = sessionStorage.getItem('categoryId');
+        
+        if(categoryId) {
+        localProducts =  products.filter(product => product.category === categoryId);
+
+        //setting active category whenver we select category from home page 
+        let listGroupItem = document.querySelector(`.list-group-item[data-id='${categoryId}']`);
+        listGroupItem.classList.add('active');
+        previousListItem = listGroupItem;
+        
+        } else {
+            localProducts = [...products];
+        }
+        for(let product of localProducts) {
+            productCardItem(product);
+        }
+    }catch(error) {
+        console.log("error", error);
+    }
+    
 }
+
+
+function filterProducts(eventData) {
+    let productList = [];
+    let target = eventData.target;
+    eventData.srcElement.classList.add('active');
+    if(target.tagName === 'BUTTON'){
+        let categoryId =  target.getAttribute('data-id');
+        productList = products.filter(product =>  product.category === categoryId)
+    }
+
+    document.querySelector('.product-container').innerHTML = '';
+    
+    for(let product of productList) {
+        productCardItem(product);
+    }
+    if(previousListItem) {
+        previousListItem.classList.remove('active');
+    }
+    previousListItem = eventData.srcElement;
+}
+
+
+
+function productCardItem(product) {
+    let productItem = document.createElement('div');
+    productItem.setAttribute('class','col-md-3');
+
+    let productCard = document.createElement('div');
+    productCard.setAttribute('class','card m-1 p-2');
+    
+    let productTitle = document.createElement('h5');
+    productTitle.setAttribute('class','card-title product-name');
+    productTitle.innerHTML = product.name;
+    productCard.append(productTitle);
+
+
+    let productImg = document.createElement('img');
+    productImg.setAttribute('src', product.imageURL);
+    productImg.setAttribute('alt', product.description);
+    productImg.setAttribute('height', '150px');
+    productImg.setAttribute('class','card-img-top');
+    productCard.append(productImg);
+
+    let productCardBody = document.createElement('div');
+    productCardBody.setAttribute('class', 'card-body');
+
+    let productDesc = document.createElement('p');
+     productDesc.setAttribute('class', 'card-text product-desc ');
+     productDesc.textContent = product.description;
+     productCardBody.append(productDesc);
+   
+     let productAction = document.createElement('div');
+     productAction.setAttribute('class', 'product-actions');
+
+     let productPrice = document.createElement("h5");
+     productPrice.setAttribute("class", "card-text");
+     productPrice.innerHTML = "MRP ₹." + product.price;
+     productAction.append(productPrice);
+    
+
+     let buyNowButton = document.createElement("button");
+     buyNowButton.setAttribute("class", "btn btnCrimsonPink");
+     buyNowButton.innerHTML = 'Buy Now';
+     productAction.append(buyNowButton);
+     
+     productCardBody.append(productAction);
+
+     productCard.append(productCardBody);
+     productItem.append(productCard);
+     document.querySelector('.product-container').appendChild(productItem);
+}
+
 
 document.addEventListener('DOMContentLoaded',loadData)
