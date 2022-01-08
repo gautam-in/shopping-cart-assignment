f53a51236760aa965d9c35673f62d7254e22d8d1
PublicisSapientSid <siddhartha.chatterjee@publicissapient.com>
2022-01-08 21:15:14 +0530

    Initialize project and create basic boilerplate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import "./App.scss"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "App"
+    }
+  };
+
+  handleClick= () => {
+    this.setState({
+      name: "Application"
+    });
+  }
+
+  render() {
+    return <div onClick={this.handleClick}>Hello {this.state.name}</div>
+  };
+};
 
 export default App;
