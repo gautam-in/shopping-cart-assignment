d68785570437883a85ec183dd3708114a7eb93e4
PublicisSapientSid <siddhartha.chatterjee@publicissapient.com>
2022-01-13 11:56:08 +0530

    Set cart dimensions

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -10,16 +10,28 @@ class Cart extends Component {
       <aside className="mini-cart-overlay">
         <div className="mini-cart">
           <header className="cart-header">
-            My Cart ({this.props.numberOfItems} item
-            {this.props.numberOfItems === 1 ? "" : "s"})
+            <h1>
+              My Cart{" "}
+              <span className="number-of-items">
+                ({this.props.numberOfItems} item
+                {this.props.numberOfItems === 1 ? "" : "s"})
+              </span>
+            </h1>
             <FaWindowClose onClick={this.props.closeCart} />
           </header>
           <section className="cart-body">
-            <ul>
-              {items.map((item) => (
-                <CartItem item={item} />
-              ))}
-            </ul>
+            {items.length ? (
+              <ul>
+                {items.map((item) => (
+                  <CartItem item={item} />
+                ))}
+              </ul>
+            ) : (
+              <div className="no-items">
+                <h1>No items in your cart</h1>
+                <p>Your favorite items are just a click away</p>
+              </div>
+            )}
           </section>
         </div>
       </aside>
