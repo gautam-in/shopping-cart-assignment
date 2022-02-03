fcfe911ce52b0569ac898cffe295fa853ce2d0ac
ck <sushmithack222@gmail.com>
2022-02-03 12:09:43 +0530

    adding app to commit

---
+++ shopping-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HomePage from './homepage.component';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
