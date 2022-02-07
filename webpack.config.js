7d67d1f05b14b75e1049d513c646f45fff35f9ec
Sourav Saha <Sourav.Saha@mlcinsurance.com>
2022-02-08 00:43:22 +0530

    Configuration changed:

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.join(__dirname, "/dist"),
-    filename: "index_bundle.js",
+    filename: "index.js",
   },
   module: {
     rules: [
