1a8537eb73facd63a665c613b7fee8da840af9d2
Yogesh Rathi <yrathi89@gmail.com>
2022-02-14 01:17:21 +0530

    Added links

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const app = express();
 port = 5000;
 
 app.use(bodyParser.json());
-app.use(express.static(process.cwd() + "/frontend/dist/sabkabazaar/"));
+app.use(express.static(process.cwd() + "/frontend/sabkabazaar/dist/sabkabazaar/"));
 app.use(express.static(process.cwd() + "/static/"));
 
 app.get('/', (req, res) => {
-    res.sendFile(process.cwd() + "/frontend/dist/sabkabazaar/index.html")
+    res.sendFile(process.cwd() + "/frontend/sabkabazaar/dist/sabkabazaar/index.html")
 });
 
 require('./routes')(app);
