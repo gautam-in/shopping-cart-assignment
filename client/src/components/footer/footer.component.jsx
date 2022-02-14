e69a59026b89aca97a8ec7bc2138ffff72e57fa0
ivaibhavtiwari <vaibhav@inevitable-infotech.com>
2022-02-14 13:36:38 +0530

    Footer component is done.

---
+++ client/src/components/footer/footer.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Container from "../container/Container";
+import { FooterContainer } from "./footer.styles";
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <Container>
+        <span>Copyright @ 2011-2018 Sabka Bazar Supplies Pvt. Ltd</span>
+      </Container>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
---
+++ client/src/components/footer/footer.styles.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const FooterContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #d8d2cb;
+  height: 50px;
+`;
