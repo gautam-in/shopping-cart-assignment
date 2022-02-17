3ce76719f2429db831dcd2ce13859226a336383f
ivaibhavtiwari <vaibhav@inevitable-infotech.com>
2022-02-17 15:27:10 +0530

    Error Boundary Added.

---
+++ client/src/components/error-boundary/error-boundary.component.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    this.setState({ hasError: true });
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    this.setState({
+      error: error,
+      errorInfo: errorInfo,
+    });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
+export default ErrorBoundary;
