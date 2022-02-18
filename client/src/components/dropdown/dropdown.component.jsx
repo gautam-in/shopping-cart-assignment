8301ec5f833fb50c7b2ace3807497d3c76f25b6f
Jay Sahoo <jaysahoo@publicisgroupe.net>
2022-02-18 18:14:08 +0530

    filter added

--- client/src/components/dropdown/dropdown.component.jsx
+++ client/src/components/dropdown/dropdown.component.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Dropdown = (props) => {
+  const navigate = useNavigate();
   return (
     <form>
-      <select name="category" id="category" style={props.style}>
+      <select
+        name="category"
+        id="category"
+        style={props.style}
+        onChange={(e) => navigate(`/products/${e.target.value}`)}
+      >
         {props.data.map((list, idx) => (
           <option value={list.key} key={idx}>
             {list.name}
