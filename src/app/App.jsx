d576279ae21a9755774d4ab190eaebe66461dda7
Rahul Parmar <rahulparmar.n@gmail.com>
2022-03-07 23:14:01 +0530

    ui added into root directly

---
+++ src/app/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+import "./style.scss";
+export const App = () => {
+  return (
+    <div id="test">
+      Test test
+      <div id="test2">SASS</div>
+    </div>
+  );
+};
+
+export default App;
