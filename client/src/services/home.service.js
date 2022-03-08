883ef3fc2cc171f55d7df402c0872ac2e64c2d2e
Darshan <gosai.darshan88@gmail.com>
2022-03-09 04:06:09 +0530

    Fixed categoruy order

--- client/src/services/home.service.js
+++ client/src/services/home.service.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
 export async function getCategories() {
+    const categories = [];
     const { data } = await axios('http://localhost:5000/categories');
-    return data;
+
+    data.filter((d) => d.order > 0).map((d) => { categories[d.order - 1] = d })
+    return categories;
+
 }
