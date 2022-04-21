b45a744888c595ceafad8704c2e08c1e109d46d5
manikumar455045 <manikumarpopuri449@gmail.com>
2022-04-21 17:09:58 +0530

    errors server request

--- client/src/redux/Banners/banners.action.js
+++ client/src/redux/Banners/banners.action.js
@@ -5,7 +5,8 @@ export const GET_BANNERS_FAILED = "GET_BANNERS_FAILED";
 export const getBanners = () => {
   return async (dispatch) => {
     try {
-      let response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/banners`);
+      let response = await axios.get(`https://sabka-0-bazaar.herokuapp.com/banners`);
+      console.log(process.env.REACT_APP_SERVER_URL);
       dispatch({
         type: GET_ALL_BANNERS,
         payload: response.data,
--- client/src/redux/Categories/categories.action.js
+++ client/src/redux/Categories/categories.action.js
@@ -4,7 +4,7 @@ export const GET_ALL_CATEGORIES = "GET_ALL_CATEGORIES";
 
 export const getAllCategories = () => {
   return async (dispatch) => {
-    let response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/categories`);
+    let response = await axios.get(`https://sabka-0-bazaar.herokuapp.com/categories`);
     dispatch({
       type: GET_ALL_CATEGORIES,
       payload: response.data,
