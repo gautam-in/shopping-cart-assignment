644536319ea59f022fcc3b63d448b0d13dbd6a7e
Wasim Khan <wasim.khan@publicissapient.com>
2022-05-04 16:21:53 +0530

    Login & Register done

---
+++ client/src/components/header/index.tsx
@@ -0,0 +1,63 @@
+// FIXME: FIX
+import React, { useContext } from 'react';
+import { useCallback, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { HOME, PRODUCTS, LOGIN, REGISTER } from '../../constants/routes';
+import AppContext from '../../contexts/appContext/app-context';
+import Cart from '../cart';
+import './index.scss';
+
+const Header = () => {
+    const [openCart, setOpenCart] = useState(false);
+    const {
+        appState: { cartCount },
+    } = useContext(AppContext);
+    const history = useHistory();
+
+    const handleRedirect = useCallback((route) => () => history.push(route), [history]);
+
+    const toggleCart = useCallback(() => setOpenCart(!openCart), [openCart]);
+
+    return (
+        <header>
+            <div className="navigation" role="navigation">
+                <div onClick={handleRedirect(HOME)}>
+                    <img className="w-1" src="static/images/logo.png" alt="logo" />
+                </div>
+                <nav>
+                    <ul className="d-flex">
+                        <li>
+                            <button onClick={handleRedirect(HOME)}>Home</button>
+                        </li>
+                        <li>
+                            <button onClick={handleRedirect(PRODUCTS)}>Products</button>
+                        </li>
+                    </ul>
+                </nav>
+                <div className="cart-login-container">
+                    <nav>
+                        <ul className="list-h d-flex">
+                            <li className="link">
+                                <button onClick={handleRedirect(LOGIN)}>Sign In</button>
+                            </li>
+                            <li className="link">
+                                <button onClick={handleRedirect(REGISTER)}>Register</button>
+                            </li>
+                        </ul>
+                    </nav>
+                    <button className="btn-cart" onClick={toggleCart}>
+                        <img src="static/images/cart.svg" alt="cart icon" className="icon" id="outside" />
+                        <span className="text" id="cart-items">
+                            {cartCount} Items
+                        </span>
+                    </button>
+                    <div id="desktop-cart" className="cart-main-cont" style={{ display: openCart ? 'block' : 'none' }}>
+                        <Cart closeCart={toggleCart} />
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
---
+++ client/src/contexts/appContext/AppState.tsx
@@ -0,0 +1,42 @@
+import React, { useReducer } from 'react';
+import { initialState, REMOVE_CART_ITEM, SET_SELECTED_CATEGORY, UPDATE_CART_ITEM } from '../../constants/actions';
+import AppContext, { ProductItem, SelectedCategory } from './app-context';
+import appReducer from './app-reducer';
+
+const AppState = ({ children }) => {
+    const [appState, dispatch] = useReducer(appReducer, initialState);
+
+    const updateCart = (item: ProductItem) => {
+        dispatch({
+            type: UPDATE_CART_ITEM,
+            payload: item,
+        });
+    };
+
+    const removeCart = (item: ProductItem) =>
+        dispatch({
+            type: REMOVE_CART_ITEM,
+            payload: item,
+        });
+
+    const setSelectedCategory = (selectedCategory: SelectedCategory) => {
+        dispatch({
+            type: SET_SELECTED_CATEGORY,
+            payload: selectedCategory,
+        });
+    };
+
+    return (
+        <AppContext.Provider
+            value={{
+                appState,
+                updateCart,
+                removeCart,
+                setSelectedCategory,
+            }}>
+            {children}
+        </AppContext.Provider>
+    );
+};
+
+export default AppState;
