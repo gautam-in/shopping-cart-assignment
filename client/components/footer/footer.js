ae16e5903cc3370223f7a9611cdb60e7f3aff135
Akshit Arora <akshitarora9211@gmail.com>
2022-05-09 22:21:03 +0530

    update footer

--- client/components/footer/footer.js
+++ client/components/footer/footer.js
@@ -1,17 +1,19 @@
 class Footer extends HTMLElement {
   constructor() {
     super();
-  }
-
-  connectedCallback() {
-    this.render();
-  }
-  render() {
-    this.innerHTML = `
-          <div style="text-align: center; font-family: sans-serif">
-            <h1>Footer</h1>
-          </div>
-          `;
+    this._shadowRoot = this.attachShadow({ mode: "open" });
+    this._shadowRoot.innerHTML = `
+      <style>
+          footer{
+              text-align: center;
+              background-color: #eaeaea;
+              padding: 3px 0px;
+          }
+      </style>
+      <footer>
+          <p>Copyright &copy; ${new Date().getFullYear()} Sabka Bazaar Grocery Suppliers Limited</p>
+      </footer>
+      `;
   }
 }
 
