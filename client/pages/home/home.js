4ae212a4588ee61ee54065c0d0edcf1fe07e8e3e
Akshit Arora <akshitarora9211@gmail.com>
2022-05-10 14:36:10 +0530

    add carousel

--- client/pages/home/home.js
+++ client/pages/home/home.js
@@ -1,3 +1,27 @@
+let curSlide = 0;
+let maxSlide = 0;
+function addListerToCarouselButtons() {
+  const prevSlide = document.querySelector(".home__btn__prev");
+  const nextSlide = document.querySelector(".home__btn__next");
+
+  nextSlide.addEventListener("click", () => {
+    if (curSlide === maxSlide - 1) {
+      curSlide = 0;
+    } else {
+      curSlide++;
+    }
+    renderBanner();
+  });
+
+  prevSlide.addEventListener("click", () => {
+    if (curSlide === 0) {
+      curSlide = maxSlide - 1;
+    } else {
+      curSlide--;
+    }
+    renderBanner();
+  });
+}
 async function fetchBannerData() {
   try {
     let response = await fetch("http://localhost:5000/banners");
@@ -18,6 +42,29 @@ async function fetchCategories() {
     return new Error("Error while fetching categories");
   }
 }
+async function renderBanner() {
+  let banners = await fetchBannerData();
+  console.log(curSlide);
+
+  if (banners.length) {
+    let bannersDOMElement = document.getElementById("banners");
+    let enabledBanners = banners.filter((banner) => banner.isActive);
+    maxSlide = enabledBanners.length;
+    enabledBanners.forEach((banner, index) => {
+      let slide = document.createElement("div");
+      slide.className = "slide";
+      slide.style.transform = `translateX(${100 * (index - curSlide)}%)`;
+
+      let slideImg = document.createElement("img");
+      slideImg.src = `../../../${banner.bannerImageUrl}`;
+      slideImg.alt = banner.bannerImageAlt;
+
+      slide.appendChild(slideImg);
+
+      bannersDOMElement.appendChild(slide);
+    });
+  }
+}
 
 async function renderCategories() {
   let categories = await fetchCategories();
@@ -32,8 +79,8 @@ async function renderCategories() {
         // category image
         let cImage = document.createElement("img");
         cImage.src = `../../..${category.imageUrl}`;
-        cImage.style.height = "150px";
-        cImage.style.width = "250px";
+        cImage.style.height = "200px";
+        cImage.style.width = "300px";
         cImage.alt = category.key;
         // category data
         let cArticle = document.createElement("article");
@@ -62,5 +109,6 @@ async function renderCategories() {
       });
   }
 }
-
+renderBanner(0);
 renderCategories();
+addListerToCarouselButtons();
