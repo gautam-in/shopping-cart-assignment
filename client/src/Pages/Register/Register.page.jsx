fb65552f46315b197ee9f330c52fd910c1e0f6fe
Vivek <vivek9142@gmail.com>
2022-05-19 19:39:02 +0530

    added designed Register component

--- client/src/Pages/Register/Register.page.jsx
+++ client/src/Pages/Register/Register.page.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import FormGroup from '../../Components/FormGroup/FormGroup.component';
-import './register.page.css';
+import {RegisterContainer,RegisterMetaContainer,RegisterHeading,RegisterMetaData,RegisterFormContainer,RegisterForm} from './register.styles.jsx';
+import { AppButton } from '../../Components/Buttons/Buttons.styles';
 
 const Register = props =>{
 
@@ -18,27 +19,39 @@ const Register = props =>{
     }
 
     return (
-        <div>
-            <form onSubmit={(ev) => {ev.preventDefault();console.log(fields)}}>
+        // <div>
 
-                <FormGroup type="text" name="firstName" onChange={handleChange} 
-                value={fields.firstName} id="firstName" text="First Name"/>
-                
-                <FormGroup type="text" name="lastName" onChange={handleChange} 
-                value={fields.lastName} id="lastName" text="Last Name"/>
+        <RegisterContainer>
+                    <RegisterMetaContainer>
+                        <RegisterHeading>Signup</RegisterHeading>
+                        <RegisterMetaData>We do not share your personal details with anyone.</RegisterMetaData>
+                    </RegisterMetaContainer>
 
-                <FormGroup type="text" name="email" onChange={handleChange} 
-                value={fields.email} id="email" text="Email"/>
+                    <RegisterFormContainer>
+                        <RegisterForm onSubmit={(ev) => {ev.preventDefault();console.log(fields)}}>
 
-                <FormGroup type="password" name="password" onChange={handleChange} 
-                value={fields.password} id="password" text="Password"/>
+                            <FormGroup type="text" name="firstName" onChange={handleChange} 
+                            value={fields.firstName} id="firstName" text="First Name"/>
+                            
+                            <FormGroup type="text" name="lastName" onChange={handleChange} 
+                            value={fields.lastName} id="lastName" text="Last Name"/>
 
-                <FormGroup type="password" name="confirmPassword" onChange={handleChange} 
-                value={fields.confirmPassword} id="confirmPassword" text="Confirm Password"/>
-                
-                <input type="submit" name="submit" />
-            </form>
-        </div>
+                            <FormGroup type="text" name="email" onChange={handleChange} 
+                            value={fields.email} id="email" text="Email"/>
+
+                            <FormGroup type="password" name="password" onChange={handleChange} 
+                            value={fields.password} id="password" text="Password"/>
+
+                            <FormGroup type="password" name="confirmPassword" onChange={handleChange} 
+                            value={fields.confirmPassword} id="confirmPassword" text="Confirm Password"/>
+
+                            <AppButton type="submit" name="submit" >Submit</AppButton>
+
+                        </RegisterForm>
+                    </RegisterFormContainer>
+                    
+                </RegisterContainer>
+        // </div>
     )
 };
 
---
+++ client/src/Pages/Register/register.styles.jsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const RegisterContainer = styled.div`
+display: flex;
+justify-content: center;
+gap: 6rem;
+margin: 4rem 0;
+`;
+
+export const RegisterMetaContainer = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+`;
+
+export const RegisterHeading = styled.h3`
+font-size:2rem;
+font-weight:700;
+margin-bottom: 1rem;
+`;
+
+export const RegisterMetaData = styled.p`
+font-size:1rem;
+font-weight:500;
+`;
+
+export const RegisterFormContainer = styled.div`
+flex-basis: 24%;
+`;
+export const RegisterForm = styled.form`
+display: flex;
+flex-direction: column;
+
+& button:last-child{
+    margin-top:1rem;
+}
+`;
