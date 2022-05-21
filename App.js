dd9f5bac1c76855711876605ae284d5eabf82d2f
unknown <lingalakasibabu029@gmail.com>
2022-05-21 17:21:29 +0530

    file

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <center>
+          <h1>Hello react world</h1>
+      </center>
+    </div>
+  )
+}
+
+export default App
