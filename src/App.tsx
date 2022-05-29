0a33fc0cc8996577dfe5f77d98324b2dca80db7e
unknown <rahulrao0209@gmail.com>
2022-05-29 11:27:38 +0530

    cleanup App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,8 @@
 import React from "react";
-import { Header } from "./components/Header/Header";
 import { Main } from "./components/Main/Main";
-import { Footer } from "./components/Footer/Footer";
 import { Layout } from "./components/Layout/Layout";
 
 export const App = () => {
-    // return (
-    //     <div>
-    //       <Header />
-    //       <Main />
-    //       <Footer />
-    //     </div>
-    // )
     return (
       <Layout>
         <Main />
