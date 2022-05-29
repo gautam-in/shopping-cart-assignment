bfc1f1e5fa6e8e25e0a79893f98fa63f37b7827a
unknown <rahulrao0209@gmail.com>
2022-05-29 11:15:31 +0530

    adding footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Header } from "./components/Header/Header";
 import { Main } from "./components/Main/Main";
+import { Footer } from "./components/Footer/Footer";
 
 export const App = () => {
     return (
         <div>
-            <Header />
-            <Main />
+          <Header />
+          <Main />
+          <Footer />
         </div>
     )
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.scss";
+
+export const Footer = () => {
+  return (
+    <footer>
+        Copyright &#169; 2022 Sabka Bazaar Grocery Supplies Pvt. Ltd.
+    </footer>
+  )
+}
