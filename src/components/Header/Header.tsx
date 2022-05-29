8b9b9b14dab26a0e62a6cd95215a57729f6525a2
unknown <rahulrao0209@gmail.com>
2022-05-29 16:59:25 +0530

    adding active link styling feature

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,34 +1,42 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import logo from "../../static/images/logo.png";
 import cart from "../../static/images/cart.svg";
 import "./Header.scss";
 
 export const Header = () => {
+    const activeLinkStyle = {
+        color: "#b3005c"
+    }
+
+    const inactiveLinkStyle = {
+        color: "#8d8d8d"
+    }
+
     return (
         <header className="header">
             
             <div className="header__logo">
-                <Link to={"/"}><img src={logo} alt="Sabka Bazaar Logo"/></Link>
+                <NavLink to={"/"}><img src={logo} alt="Sabka Bazaar Logo"/></NavLink>
             </div>
 
             <nav className="header__nav">
 
                 <div className="nav--left">
-                 <Link to={"/"}>Home</Link>
-                 <Link to={"/products"}>Products</Link>
+                 <NavLink to={"/"} style={ ({ isActive }) => isActive ? activeLinkStyle : inactiveLinkStyle }>Home</NavLink>
+                 <NavLink to={"/products"} style={ ({ isActive }) => isActive ? activeLinkStyle : inactiveLinkStyle }>Products</NavLink>
                 </div>
 
                 <div className="nav--right">
-                    <Link to={""}>SignIn</Link>
-                    <Link to={""}>Register</Link>
+                    <NavLink to={""}>SignIn</NavLink>
+                    <NavLink to={""}>Register</NavLink>
                 </div>
 
                 <div className="nav--cart">
-                    <Link to={""}>
+                    <NavLink to={""}>
                       <div className="cart"><img src={cart} alt="cart"/></div>
                       <div>0 items</div>
-                    </Link>
+                    </NavLink>
                 </div>
 
             </nav>
