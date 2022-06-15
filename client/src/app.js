4782bd03730037da2ced8ce83b568c7f4a5a8aa2
Arshad Ansari <arsansar@publicisgroupe.net>
2022-06-15 16:26:29 +0530

    sabka bazar client v1

--- client/src/app.js
+++ client/src/app.js
@@ -2,20 +2,6 @@ var cart = document.getElementsByTagName("svg");
 var button = document.getElementsByTagName("button");
 
 
-// for(var but of button){
-//     let count = 0;
-//     but.addEventListener("click", (e)=>{
-//         count++;
-//         console.log('add to cart clicked', count++)
-//         document.getElementById("cart-item").innerHTML = `${count} item`
-
-
-//         var add = Number(cart.getAttribute("data-count") || 0);
-//         cart.setAttribute("data-count", add +1)
-//         cart.classList.add("zero")
-//     })
-// }
-
 //Adding items to the cart
 
 let count = 0;
