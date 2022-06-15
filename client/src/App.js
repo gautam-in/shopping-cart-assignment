100d205c81cc9f1ddcf71251984919021d865c29
saniketprdxn <anikets@axioned.com>
2022-06-15 16:49:35 +0530

    feat: initial commit

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Homepage from './pages/Homepage';
+
+function App() {
+  return (
+    <div className="container">
+      <Homepage />
+    </div>
+  );
+}
+export default App;
