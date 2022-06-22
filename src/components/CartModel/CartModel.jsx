15f2146f814dc486ac28c30c7afc88e586f78482
Sharvin T <shat@WKMZT1D07FCE.global.publicisgroupe.net>
2022-06-22 18:01:25 +0530

    Added Mini cart and Overlays

---
+++ src/components/CartModel/CartModel.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import "../CartModel/CartModel.scss";
+import Button from "../Button/Button";
+import { MdNavigateNext } from "react-icons/md";
+
+const CartModel = ({ handleCartOpen }) => {
+  const product = {
+    name: "Apple - Washington, Regular, 4 pcs",
+    imageURL: "/static/images/products/fruit-n-veg/apple.jpg",
+    description:
+      "The bright red coloured and heart shaped Washington apples are crunchy, juicy and slightly sweet. Washington Apples are a natural source of fibre and are fat free.",
+    price: 187,
+    stock: 50,
+    category: "5b6899953d1a866534f516e2",
+    sku: "fnw-apple-4",
+    id: "5b6c6aeb01a7c38429530884",
+  };
+  return (
+    <div className="CartModel-container">
+      <div className="cartModel">
+        {/* header */}
+        <header className="cartModel-header-container">
+          <div className="cartModel-header-left">
+            My Cart <span>(1 item)</span>
+          </div>
+          <div className="cartModel-header-right" onClick={handleCartOpen}>
+            x
+          </div>
+        </header>
+        {/* itmes */}
+        <div className="cartModel-itmes-container">
+          <img
+            className="cartModel-itmes-image"
+            src={product.imageURL}
+            alt="product"
+          />
+          <div className="cartModel-name-price-container">
+            <div className="cartModel-itmes-name">{product.name}</div>
+            <div className="cartModel-itmes-price">
+              <div className="cartModel-itmes-price-calculator">
+                <button className="cartModel-price-button">-</button>1
+                <button className="cartModel-price-button">+</button>
+                <span>x</span>
+                Rs{product.price}
+              </div>
+              <span>Rs{product.price}</span>
+            </div>
+          </div>
+        </div>
+        {/* banner */}
+        <div className="cartModel-priceBanner">
+          <img src="/static/images/lowest-price.png" alt="lowest-price png" />
+          <p>You won't find it cheaper anywhere</p>
+        </div>
+        {/* checkout */}
+        <div className="cartModel-checkout-container">
+          <span className="promo">
+            Promo code can be applied on payment page.
+          </span>
+          <Button type="checkout">
+            <span>Proceed to cart</span>
+            <div className="cart-proceed">
+              <span>Rs.{187}</span>
+              <MdNavigateNext />
+            </div>
+          </Button>
+          {/* <Button type="sign">Start Shopping</Button> */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartModel;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 import "../Header/Header.scss";
 import { MdShoppingCart } from "react-icons/md";
+import CartModel from "../CartModel/CartModel";
 
 const Header = () => {
+  const [isCartOpen, setIsCartOpen] = useState(false);
+
+  const handleCartOpen = () => {
+    console.log("click");
+    setIsCartOpen(!isCartOpen);
+    isCartOpen
+      ? (document.getElementById("overlay").style.display = "none")
+      : (document.getElementById("overlay").style.display = "block");
+
+    isCartOpen
+      ? (document.body.style.overflow = "unset")
+      : (document.body.style.overflow = "hidden");
+  };
   return (
     <>
+      <div id="overlay"></div>
       <header className="header-container">
         <div className="image-container">
           <img
@@ -33,13 +48,14 @@ const Header = () => {
               </Link>
             </div>
             <div className="products-cart-container">
-              <div className="cart">
+              <div className="cart" onClick={handleCartOpen}>
                 <MdShoppingCart className="cart-icon" />0 Items
               </div>
             </div>
           </div>
         </div>
       </header>
+      {isCartOpen && <CartModel handleCartOpen={handleCartOpen} />}
       <Outlet />
     </>
   );
