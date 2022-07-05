cdcc3675bbb85e66630ee7a9d0a8db2ff3446243
Developer Raj <raajjj.gh@gmail.com>
2022-07-05 18:06:02 +0530

    Updated test cases

--- src/__test__/header.test.js
+++ src/__test__/header.test.js
@@ -4,65 +4,67 @@ import userEvent from "@testing-library/user-event";
 import { BrowserRouter } from "react-router-dom";
 import Header from "../components/Header";
 
-test("Is header contains logo", async () => {
-  render(
-    <BrowserRouter>
-      <Header cartCounter={0} />
-    </BrowserRouter>
-  );
-  expect(await screen.findByAltText(/sabka bazaar logo/i)).toHaveAttribute(
-    "src",
-    "logo.png"
-  );
-});
-
-test("Is header contains home, product, signin, and register, link", async () => {
-  render(
-    <BrowserRouter>
-      <Header cartCounter={0} />
-    </BrowserRouter>
-  );
-  const homeLink = screen.getAllByRole("link", { name: /home/i });
-  const signinLink = screen.getAllByRole("link", { name: /sign in/i });
-  const registerLink = screen.getAllByRole("link", { name: /register/i });
-  const productLink = screen.getAllByRole("link", { name: /products/i });
-
-  // Checking all home link's have proper href and name
-  homeLink.forEach((item) => {
-    expect(item).toHaveAttribute("href", "/");
+describe("Header", () => {
+  test("Is header contains logo", async () => {
+    render(
+      <BrowserRouter>
+        <Header cartCounter={0} />
+      </BrowserRouter>
+    );
+    expect(await screen.findByAltText(/sabka bazaar logo/i)).toHaveAttribute(
+      "src",
+      "logo.png"
+    );
   });
-  // Checking all sign in link's have proper href and name
-  signinLink.forEach((item) => {
-    expect(item).toHaveAttribute("href", "/signin");
-  });
-  // Checking all sign in register link's have proper href and name
-  registerLink.forEach((item) => {
-    expect(item).toHaveAttribute("href", "/register");
-  });
-  //   Checking product link have proper href and name;
-  productLink.forEach((item) => {
-    expect(item).toHaveAttribute("href", "/products");
-  });
-});
 
-test("Header contains cart button", () => {
-  render(
-    <BrowserRouter>
-      <Header cartCounter={0} />
-    </BrowserRouter>
-  );
-  const cartBtn = screen.getByTestId("cart-button");
-  expect(cartBtn).toBeInTheDocument();
-});
+  test("Is header contains home, product, signin, and register, link", async () => {
+    render(
+      <BrowserRouter>
+        <Header cartCounter={0} />
+      </BrowserRouter>
+    );
+    const homeLink = screen.getAllByRole("link", { name: /home/i });
+    const signinLink = screen.getAllByRole("link", { name: /sign in/i });
+    const registerLink = screen.getAllByRole("link", { name: /register/i });
+    const productLink = screen.getAllByRole("link", { name: /products/i });
 
-test("Contains active class when cart button is clicked", () => {
-  render(
-    <BrowserRouter>
-      <Header cartCounter={0} />
-    </BrowserRouter>
-  );
-  const cartBtn = screen.getByTestId("cart-button");
-  userEvent.click(cartBtn).then(() => {
-    expect(cartBtn).toHaveAttribute("class", /active/i);
+    // Checking all home link's have proper href and name
+    homeLink.forEach((item) => {
+      expect(item).toHaveAttribute("href", "/");
+    });
+    // Checking all sign in link's have proper href and name
+    signinLink.forEach((item) => {
+      expect(item).toHaveAttribute("href", "/signin");
+    });
+    // Checking all sign in register link's have proper href and name
+    registerLink.forEach((item) => {
+      expect(item).toHaveAttribute("href", "/register");
+    });
+    //   Checking product link have proper href and name;
+    productLink.forEach((item) => {
+      expect(item).toHaveAttribute("href", "/products");
+    });
+  });
+
+  test("Header contains cart button", () => {
+    render(
+      <BrowserRouter>
+        <Header cartCounter={0} />
+      </BrowserRouter>
+    );
+    const cartBtn = screen.getByTestId("cart-button");
+    expect(cartBtn).toBeInTheDocument();
+  });
+
+  test("Contains active class when cart button is clicked", () => {
+    render(
+      <BrowserRouter>
+        <Header cartCounter={0} />
+      </BrowserRouter>
+    );
+    const cartBtn = screen.getByTestId("cart-button");
+    userEvent.click(cartBtn).then(() => {
+      expect(cartBtn).toHaveAttribute("class", /active/i);
+    });
   });
 });
--- src/__test__/register.test.js
+++ src/__test__/register.test.js
@@ -3,48 +3,50 @@ import { screen, render } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import Register from "../pages/Register";
 
-test("Register page contains form element", () => {
-  render(
-    <BrowserRouter>
-      <Register />
-    </BrowserRouter>
-  );
+describe("Registration", () => {
+  test("Register page contains form element", () => {
+    render(
+      <BrowserRouter>
+        <Register />
+      </BrowserRouter>
+    );
 
-  const form = screen.getByRole("form");
-  expect(form).toBeInTheDocument();
-});
-
-test("Register page contains form elements and it contains required input fields with type: email, and password", () => {
-  render(
-    <BrowserRouter>
-      <Register />
-    </BrowserRouter>
-  );
-
-  const firstName = screen.getByLabelText(/first name/i);
-  const lastName = screen.getByLabelText(/last name/i);
-  const email = screen.getByLabelText(/email/i);
-  const passwords = screen.getAllByLabelText(/password/i);
-
-  // First Name
-  expect(firstName).toHaveAttribute("type", "text");
-  expect(firstName).toHaveAttribute("class", "input-fields");
-  // First Name
-
-  // Last Name
-  expect(lastName).toHaveAttribute("type", "text");
-  expect(lastName).toHaveAttribute("class", "input-fields");
-  // Last Name
-
-  // Email
-  expect(email).toHaveAttribute("type", "email");
-  expect(email).toHaveAttribute("class", "input-fields");
-  // Email
-
-  // Passwords
-  passwords.forEach((password) => {
-    expect(password).toHaveAttribute("type", "password");
-    expect(password).toHaveAttribute("class", "input-fields");
+    const form = screen.getByRole("form");
+    expect(form).toBeInTheDocument();
   });
-  // Passwords
+
+  test("Register page contains form elements and it contains required input fields with type: email, and password", () => {
+    render(
+      <BrowserRouter>
+        <Register />
+      </BrowserRouter>
+    );
+
+    const firstName = screen.getByLabelText(/first name/i);
+    const lastName = screen.getByLabelText(/last name/i);
+    const email = screen.getByLabelText(/email/i);
+    const passwords = screen.getAllByLabelText(/password/i);
+
+    // First Name
+    expect(firstName).toHaveAttribute("type", "text");
+    expect(firstName).toHaveAttribute("class", "input-fields");
+    // First Name
+
+    // Last Name
+    expect(lastName).toHaveAttribute("type", "text");
+    expect(lastName).toHaveAttribute("class", "input-fields");
+    // Last Name
+
+    // Email
+    expect(email).toHaveAttribute("type", "email");
+    expect(email).toHaveAttribute("class", "input-fields");
+    // Email
+
+    // Passwords
+    passwords.forEach((password) => {
+      expect(password).toHaveAttribute("type", "password");
+      expect(password).toHaveAttribute("class", "input-fields");
+    });
+    // Passwords
+  });
 });
