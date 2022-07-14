bd9e2d6ca67f1e0c7430d9ff7aa0957164b447e6
Subhashree Patra <shreesubha.p@gmail.com>
2022-07-14 15:29:40 +0530

    Login

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,41 +1,11 @@
 import React, { useState } from "react";
 import { Col, Container, Button, Form } from "react-bootstrap";
 import "./login.css";
-//import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  //const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  //   const getUserData = async () => {
-  //     const res = await fetch("http://localhost:3000/users");
-  //     const json = await res.json();
-  //     loginCheck(json);
-  //   };
-
-  //   const loginCheck = (loginRecords) => {
-  //     if (email.length > 0 && password.length > 0) {
-  //       const findRes = loginRecords.find((item) => {
-  //         if (item.email === email && item.confirm_password === password) {
-  //           sessionStorage.setItem("userLoggedIn", true);
-  //           navigate("/home");
-  //           return true;
-  //         }
-  //         return false;
-  //       });
-  //       if (!findRes) {
-  //         toast("Please enter correct details");
-  //       }
-  //     } else {
-  //       toast("Please enter email id and password");
-  //     }
-  //   };
-
-  //   const handleLogin = (e) => {
-  //     e.preventDefault();
-  //     getUserData();
-  //   };
   return (
     <Container className="d-flex justify-content mt-4">
       <Col>
