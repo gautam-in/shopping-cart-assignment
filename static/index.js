cea5b6f5d4b030a0dfd19cdffa7326c2e4cbe758
Ravi Singh <ravsingh20@publicisgroupe.net>
2022-08-08 01:43:41 +0530

    implemented navigation for Home,Products,Login and Reigster links

--- static/index.js
+++ static/index.js
@@ -1,10 +1,14 @@
+
+const UI = Model('hidden')
 document.addEventListener('DOMContentLoaded', () => {
     //(alert,0,'Loaded');
+    UI('home');
     fetch('http://localhost:5000/categories').then((resp) => {
         resp.json().then(data => {
             ShowBanners(data);
         })
     })
+    HandleInputAttribute();
 });
 
 function ShowBanners(categories) {
@@ -14,7 +18,7 @@ function ShowBanners(categories) {
         return b.order < a.order ? 1 : -1;
     });
 
-    let template = document.querySelector("div[template]");
+    let template = document.querySelector("#categories div[template]");
     for (let i = 0; i < filtered.length; i++) {
         const jsonItem = filtered[i];
         let item = template.cloneNode(true);
@@ -33,7 +37,7 @@ function ShowBanners(categories) {
         desc.append(jsonItem.description)
 
         let link = item.querySelector('img+div>div a.desc-item');
-        
+
         //not sure about, may need to change according the approach of navigating different page
         link.setAttribute('href', "Categories/" + jsonItem.key);
 
@@ -41,4 +45,72 @@ function ShowBanners(categories) {
 
         template.parentElement.appendChild(item);
     }
+}
+
+function HandleInputAttribute() {
+    document.querySelectorAll('.m-control input.m-input').forEach(input => {
+        input.addEventListener('input', (e) => {
+            if (e.target.value.length) {
+                e.target.setAttribute('hasvalue', 'true')
+            } else {
+                e.target.removeAttribute('hasvalue')
+            }
+            return true;
+        })
+    });
+}
+
+function Model(css) {
+    let _ = document.querySelector.bind(document);
+    const elements = {
+        products: 'section.plp',
+        login: 'section.login',
+        home: ['section#offers-area', 'section#categories'],
+        signup: 'section.login + section.login'
+    }
+    let all = [];
+    for (const n in elements) {
+        if (Object.hasOwnProperty.call(elements, n)) {
+            const element = elements[n];
+            all.push(element);
+        }
+    }
+    all = all.flat(2);
+
+    function _hideAll() {
+        all.forEach(a => {
+            /**
+             * @type {HTMLTableSectionElement}
+             */
+            let elem = _(a);
+            if (elem) {
+                elem.classList.remove(css);//to make sure there is only one
+                elem.classList.add(css);
+            }
+        });
+    }
+
+    function showOne(selector) {
+        /**@type {HTMLTableSectionElement}*/
+        let elem = _(selector);
+        if (elem) {
+            elem.classList.remove(css);
+        }
+    }
+
+    function Show(name) {
+        let items = elements[name.toLowerCase()];
+        if (items) {
+            if (Array.isArray(items) && items.length) {
+                _hideAll();
+                items.forEach(a => showOne(a));
+            } else if (typeof items === "string" && items) {
+                _hideAll();
+                showOne(items);
+            } else {
+                console.assert(flase, "unsopported format")
+            }
+        }
+    }
+    return Show;
 }
