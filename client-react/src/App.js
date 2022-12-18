caa3cb686168e75961a5813fb15d1538c1eed5b3
Alisha Salgaonkar <alisalga@publicisgroupe.net>
2022-12-18 22:10:55 +0530

    Worked on header design

--- client-react/src/App.js
+++ client-react/src/App.js
@@ -3,17 +3,20 @@ import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Homepage, ProductListPage } from './pages';
+import { Header } from './features/header/Header';
 
 function App() {
   return (
     <div className="App">
       <div className='container app-container'>
+
         <Suspense fallback={<div>Loading...</div>}>
           <Router>
+          <Header/>
             <Routes>
               <Route path="/" element={<Homepage />} />
-              <Route path="/categories" element={<ProductListPage/>} exact />
-            <Route path="/categories/:id" element={<ProductListPage/>} exact />
+              <Route path="/products" element={<ProductListPage/>} exact />
+            <Route path="/products/:id" element={<ProductListPage/>} exact />
             {/* <Route path="/signin" element={Signin} exact />
             <Route path="/signup" element={Signup} exact /> */}
             </Routes>
--- client-react/src/features/header/Header.js
+++ client-react/src/features/header/Header.js
@@ -1,7 +1,39 @@
 import React from 'react'
+import Nav from 'react-bootstrap/Nav';
+import { Link } from "react-router-dom";
+import Navbar from 'react-bootstrap/Navbar';
+import './Header.scss';
 
 export const Header = () => {
+  const logoURL = '/static/images/logo.png';
+  const cartImageURL = '/static/images/cart.svg';
+  const BASE_URL = "http://127.0.0.1:5500";
+
   return (
-    <div>Header</div>
+    <Navbar expand="lg" className='p-0 justify-content-between'>
+      <Navbar.Brand href="#"><img src={BASE_URL + logoURL} alt="company logo" width="125px" /></Navbar.Brand>
+      <Nav className="flex-row page-nav">
+          <Nav.Item>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link as={Link} to="/products">Products</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      <div className='justify-content-end '>
+        <Nav className="flex-row">
+          <Nav.Item>
+            <Nav.Link href="/signin">Sign In</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="/register">Register</Nav.Link>
+          </Nav.Item>
+        </Nav>
+        <button className='cart-btn'>
+          <img src={BASE_URL + cartImageURL} width="16px" alt='Cart Icon' />Items
+          {/* {cartList.length} {cartList.length <= 1 ? 'item' : 'items'} */}
+        </button>
+      </div>
+    </Navbar>
   )
 }
