b737a839c603f9c057a191b46d6a945b4efec6f8
Ranganth MD <ranganath.devaramani@publicissapient.com>
2023-01-23 19:54:01 +0530

    Added Header component

--- client/src/components/Layout/Footer.tsx
+++ client/src/components/Layout/Footer.tsx
@@ -2,6 +2,8 @@ import styles from "./layout.module.scss"
 
 export const Footer = () => {
   return (
-    <div className={styles.footer}>Copyright@ 2011-2018 Sabka Bazaar Grocery Suppliers Pvt Ltd</div>
-  )
+    <footer className={styles.footer}>
+      Copyright@ 2011-2018 Sabka Bazaar Grocery Suppliers Pvt Ltd
+    </footer>
+  );
 }
--- client/src/components/Layout/Header.tsx
+++ client/src/components/Layout/Header.tsx
@@ -1,7 +1,35 @@
 import styles from './layout.module.scss'
+import LogoImage from "../../static/images/logo.png"
+import CartImage from "../../static/images/cart.svg"
 
+const Logo = () => {
+  return (
+    <img src={LogoImage} alt="Logo of Sabka Bazaar" />
+  )
+}
 export const Header = () => {
   return (
-    <div className={styles.header}>Header</div>
-  )
+    <div className={styles.header_container}>
+      <header className={styles.header}>
+        <Logo />
+        <nav className={styles.nav_links}>
+          <a href="#">Home</a>
+          <a href="#">Products</a>
+        </nav>
+        <div>
+          <div className={styles.right_nav}>
+            <nav className={styles.nav_links_auth}>
+              <a href="#">SignIn</a>
+              <a href="#">Register</a>
+            </nav>
+
+            <button>
+              <img src={CartImage} alt="" />
+              <span>0 items</span>
+            </button>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
 }
