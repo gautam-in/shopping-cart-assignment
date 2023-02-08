ccd68cf0e81d9ab912f093791eec0517f2f9bd41
Pranoy Roy <praroy4@WKMZT5FEDB5C.global.publicisgroupe.net>
2023-02-08 11:47:35 +0530

    feat: added product filter

--- client/src/components/products/ProductsFilter.jsx
+++ client/src/components/products/ProductsFilter.jsx
@@ -1,8 +1,14 @@
 import queryData from "@/src/utils/queryData";
+import { useState } from "react";
 import { useQuery } from "react-query";
-import styles from "./ProductsContainer.module.scss";
+import styles from "./ProductsFilter.module.scss";
 
 export default function ProductsFilter() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(
+    "Please Choose Category..."
+  );
+
   const { data: categories } = useQuery({
     queryKey: ["categories"],
     queryFn: queryData,
@@ -12,22 +18,50 @@ export default function ProductsFilter() {
     .sort((a, b) => a.order - b.order)
     .filter((item) => item.order > 0);
 
+  const handleBtnClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleOptionClick = (e) => {
+    setSelectedOption(e.target.id);
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
       <div className={styles.sideFilter}>
         {validCategories.map((item) => (
-          <a href={`#${item.name}`}>{item.name}</a>
+          <a key={item.id} href={`#${item.name}`}>
+            {item.name}
+          </a>
         ))}
       </div>
       <div className={styles.selectFilter}>
-        <select name="categories" id="categories">
-          <option disabled="">
-            Please choose...
-          </option>
+        <button onClick={handleBtnClick} className={styles.ddBtn}>
+          <span>{selectedOption}</span>
+          {isOpen ? (
+            <i className={`${styles.arrow} ${styles.up}`} />
+          ) : (
+            <i className={`${styles.arrow} ${styles.down}`} />
+          )}
+        </button>
+        <div
+          onClick={handleOptionClick}
+          className={
+            isOpen ? styles.filterContentShow : styles.filterContentHide
+          }
+        >
           {validCategories.map((item) => (
-            <option value={item.name}>{item.name}</option>
+            <a
+              key={item.id}
+              id={item.name}
+              href={`#${item.name}`}
+              className={styles.contentItem}
+            >
+              {item.name}
+            </a>
           ))}
-        </select>
+        </div>
       </div>
     </>
   );
