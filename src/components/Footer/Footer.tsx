379d3c477518b181c181d16fc30c9422d140af27
Robin Singh <robinsingh1912@gmail.com>
2023-02-08 17:38:16 +0530

    Footer added

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,9 @@
 export default function Footer() {
   return (
-    <footer className="text-3xl font-bold underline">
-      Copyright &copy; 2011-2018 Sabka Bazaar Grocery Supplies Pvt. Ltd
+    <footer className="bg-gray-200">
+      <div className="py-2 max-w-5xl mx-auto text-center lg:text-left ">
+        Copyright &copy; 2011-2018 Sabka Bazaar Grocery Supplies Pvt. Ltd
+      </div>
     </footer>
   );
 }
--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -2,10 +2,10 @@ import Footer from "../components/Footer/Footer";
 
 export default function RootLayout({ children }: React.PropsWithChildren) {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <header>hello header</header>
-      <main>{children}</main>
+      <main className="flex-1">{children}</main>
       <Footer />
-    </>
+    </div>
   );
 }
