ce20ff0968efedbd471e9922d5566a9ef3188ad8
vipin rawat <viprawat2@publicisgroupe.net>
2023-02-13 19:35:01 +0530

    minor fix

--- client/src/components/login/LoginForm.tsx
+++ client/src/components/login/LoginForm.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import useAppStore from '../../utils/store/store';
