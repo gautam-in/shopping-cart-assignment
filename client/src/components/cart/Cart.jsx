2f57ac70dc6f40d5d598f82d2f499faa26a7509d
Pranoy <pranoy.roy@publicissapient.com>
2023-02-13 15:18:08 +0530

    feat: added empty cart and cart banner ui

--- client/src/components/cart/Cart.jsx
+++ client/src/components/cart/Cart.jsx
@@ -1,6 +1,7 @@
 import { useCartState } from "@/src/hooks/useCartContext";
 import { useRouter } from "next/router";
 import styles from "./Cart.module.scss";
+import CartBanner from "./CartBanner";
 import CartItem from "./CartItem";
 
 export default function Cart() {
@@ -13,28 +14,41 @@ export default function Cart() {
   };
 
   const renderCartItems = () => {
-    return cartState.totalItems
-      ? Object.values(cartState.items).map((item) => <CartItem item={item} />)
-      : null;
+    return cartState.totalItems ? (
+      <div>
+        {Object.values(cartState.items).map((item) => (
+          <CartItem item={item} />
+        ))}
+        <CartBanner />
+      </div>
+    ) : (
+      <div className={styles.emptyCartContainer}>
+        <p>No Items in your cart</p>
+        <p>Your favorite items are just</p>
+      </div>
+    );
   };
 
   const renderCartBtn = () => {
     if (cartState.totalItems === 0) {
       return (
         <button id="continue" className={styles.cartContinueBtn}>
-          <span>Continue Shopping</span>
+          <span>Start Shopping</span>
         </button>
       );
     }
 
     return (
-      <button id="checkout" className={styles.cartCheckoutBtn}>
-        <span>Proceed to Checkout</span>
-        <span>
-          <span>Rs.{cartState.totalCartPrice}</span>
-          <i className={styles.arrow} />
-        </span>
-      </button>
+      <>
+        <span className={styles.promoText}>Promo code can be applied on payment page</span>
+        <button id="checkout" className={styles.cartCheckoutBtn}>
+          <span>Proceed to Checkout</span>
+          <span>
+            <span>Rs.{cartState.totalCartPrice}</span>
+            <i className={styles.arrow} />
+          </span>
+        </button>
+      </>
     );
   };
 
@@ -44,10 +58,14 @@ export default function Cart() {
 
   return (
     <div className={styles.container}>
-      <div className={styles.cartTitle}>
-        <h3>{cartTitleText}</h3>
+      <div
+        className={
+          cartState.totalItems === 0 ? styles.emptyCartTitle : styles.cartTitle
+        }
+      >
+        <h3>{cartState.totalItems === 0 ? "My Cart" : cartTitleText}</h3>
       </div>
-      <div className={styles.cartItems}>{renderCartItems()}</div>
+      {renderCartItems()}
       <div className={styles.cartFooter} onClick={handleCartFooterBtnClick}>
         {renderCartBtn()}
       </div>
