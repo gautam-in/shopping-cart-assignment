99feff5e287445d1fe3cb41cc347842acbfe6206
Robin Singh <robinsingh1912@gmail.com>
2023-02-16 23:01:32 +0530

    modal created

--- src/components/CartModal/CartModal.tsx
+++ src/components/CartModal/CartModal.tsx
@@ -1,9 +1,22 @@
+import { useEffect } from "react";
 import { useCartContext } from "../../context/cartContext";
 
 interface ICartModal {
   onClose: () => void;
 }
 export default function CartModal({ onClose }: ICartModal) {
+  useEffect(() => {
+    const close = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        onClose();
+      }
+    };
+    window.addEventListener("keydown", close);
+    return () => window.removeEventListener("keydown", close);
+  }, []);
+
+  const { state } = useCartContext();
+
   return (
     <div
       className="z-50 block fixed left-0 top-0 h-full w-full bg-black/40"
@@ -13,7 +26,8 @@ export default function CartModal({ onClose }: ICartModal) {
         <div className="absolute bottom-0 right-0 w-1/2">
           <div className="bg-black p-4 text-white flex justify-between">
             <div className="">
-              My Cart (<span className="text-xs">{1} items</span>)
+              My Cart (
+              <span className="text-xs">{state.cart.length} items</span>)
             </div>
             <button onClick={onClose}>X</button>
           </div>
