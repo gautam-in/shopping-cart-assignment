b35a0ee02772252d8cb51d73f5240ee3faf007f6
Robin Singh <robinsingh1912@gmail.com>
2023-02-17 13:47:43 +0530

    sidebar fixed

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,10 +1,10 @@
-import Link from "next/link";
 import { useCategories } from "../../hooks";
 import RootLayout from "../../layouts/Layout";
 import { useRouter } from "next/router";
 import { useEffect, useMemo, useState } from "react";
 import { getProducts, TProduct } from "../../apis/products";
 import { useCartContext } from "../../context/cartContext";
+import { SideBar } from "../../components/SideBar/SideBar";
 
 export default function ProductsPage() {
   return (
@@ -78,40 +78,5 @@ export function ProductCard(props: TProduct) {
         </button>
       </div>
     </article>
-  );
-}
-
-export function SideBar() {
-  const [show, setShow] = useState(false);
-  const { categories } = useCategories();
-  const router = useRouter();
-  const {
-    query: { category },
-  } = router;
-
-  return (
-    <aside className="bg-offWhite flex flex-col">
-      <button
-        className="bg-primary text-white p-4 flex justify-between sm:hidden"
-        onClick={() => setShow((prev) => !prev)}
-      >
-        {category || "Select Category"}
-        <span className={"transition-all " + (show ? "rotate-180" : "")}>
-          &#x2304;
-        </span>
-      </button>
-      <ul className={"w-full " + (show ? "block" : "hidden sm:block")}>
-        {categories.map((category) => (
-          <li
-            className="px-6 py-2 border-b-2 border-black/5 text-black/75"
-            key={category.id}
-          >
-            <Link href={`/products?category=${category.key}`}>
-              {category.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </aside>
   );
 }
