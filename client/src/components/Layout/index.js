f935bcc2324b6515f44cce89f5ef57d4be543a33
Roshan Kumar <roskumar8@WKMZT7DAA228.global.publicisgroupe.net>
2023-03-14 10:18:18 +0530

    refactored the code & documentation added

--- client/src/components/Layout/index.js
+++ client/src/components/Layout/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { useShopContext } from '../../context';
 import { Outlet } from 'react-router-dom';
 import { Footer } from './Footer';
 import { Header } from './Header';
 import classes from './layout.module.scss';
 import { Cart } from '../../pages';
 
+/**
+ * The Layout function returns the Cart, Header, Outlet, and Footer components
+ * @returns The return statement is returning the JSX that is being rendered.
+ */
 export const Layout = () => {
-  const { isCartOpen } = useShopContext();
-
   return (
     <>
       <Cart />
-      <div style={{ position: isCartOpen ? 'fixed' : 'inherit', width: '100%' }}>
+      <div >
         <Header />
         <section className={classes.body}>
           <Outlet />
--- client/src/components/ProductsCategories/index.js
+++ client/src/components/ProductsCategories/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import styles from './category.module.scss';
 
+/**
+ * It takes in a category object and an index number, and returns a div with a class name that depends
+ * on the index number, and a nested div with an image and a nested div with a heading, a paragraph,
+ * and a button
+ * @returns A div with a class of card or card_reverse depending on the index of the category.
+ */
 export const CategoryCard = ({ category, index }) => {
+  const { name, description, imageUrl, key } = category;
   return (
     <div className={index % 2 === 0 ? styles.card : styles.card_reverse}>
       <div className={styles.image_container}>
-        <img src={category.imageUrl} alt={category.description} />
+        <img src={imageUrl} alt={description} />
       </div>
       <div className={styles.category_details}>
-        <h2>{category.name}</h2>
-        <p>{category.description}</p>
-        <button>Explore {category.key}</button>
+        <h2>{name}</h2>
+        <p>{description}</p>
+        <button>Explore {key}</button>
       </div>
     </div>
   );
