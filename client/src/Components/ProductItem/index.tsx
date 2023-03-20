ba6d479793e876589a181e4b1e68be69ffe994ee
Akshay Kulkarni <Akshay.Kulkarni@publicissapient.com>
2023-03-20 12:40:58 +0530

    finished PLP

--- client/src/Components/ProductItem/index.tsx
+++ client/src/Components/ProductItem/index.tsx
@@ -1,7 +1,56 @@
+import { Button } from "../Common"
+
 import { ProductItemProps } from "./models"
 
+import "./styles.scss"
+
 export const ProductItem = ({
-  product: { name, description, price, imageURL },
+  product: { id, name, description, price, imageURL },
 }: ProductItemProps) => {
-  return <div className="product-item">{name}</div>
+  return (
+    <div className="product-item">
+      <h4 className="name">{name}</h4>
+
+      <div className="details">
+        <img src={imageURL} alt={name} className="image" />
+
+        <div>
+          <p className="description">
+            {description.substring(0, 150)}
+            {description.length > 150 && "..."}
+          </p>
+          <Button
+            type="button"
+            variant="primary"
+            classes="action-btn-phone"
+            handleClick={() => console.log(id)}
+          >
+            Buy Now @ MRP Rs. {price}
+          </Button>
+        </div>
+      </div>
+
+      <div className="actions flex">
+        <span className="price">MRP Rs. {price}</span>
+
+        <Button
+          type="button"
+          variant="primary"
+          classes="action-btn-desktop"
+          handleClick={() => console.log(id)}
+        >
+          Buy Now
+        </Button>
+
+        <Button
+          type="button"
+          variant="primary"
+          classes="action-btn-tab"
+          handleClick={() => console.log(id)}
+        >
+          Buy Now @ Rs. {price}
+        </Button>
+      </div>
+    </div>
+  )
 }
