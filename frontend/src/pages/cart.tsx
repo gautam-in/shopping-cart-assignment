957b8e84a977f607838cd8b756866f8924726937
“Manoj <manoj.mukherjee@publicissapient.com>
2023-03-29 18:23:49 +0530

    fix: hydration issue

--- frontend/src/pages/cart.tsx
+++ frontend/src/pages/cart.tsx
@@ -1,5 +1,9 @@
-import CartBody from "@/components/Cart/CartBody";
+import dynamic from "next/dynamic";
 import React from "react";
+
+const CartBody = dynamic(() => import("@/components/Cart/CartBody"), {
+  ssr: false,
+});
 
 const CartPage = () => {
   return <CartBody />;
