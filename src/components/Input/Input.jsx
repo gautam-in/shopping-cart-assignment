4c1c6d902c984faca0f97c11950b49996f240fe9
Harikrishnan Kr <harkr@WKMZT728D67C.global.publicisgroupe.net>
2023-04-19 14:23:01 +0530

    Added login page

---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./Input.module.scss";
+import { useController } from "react-hook-form";
+
+function Input(props) {
+
+    const {
+        label,
+        name,
+        type,
+        ...rest
+    } = props;
+
+    const { field, fieldState } = useController(props);
+
+    return (
+        <div className={`${styles.input} ${fieldState.invalid ? styles.invalid : ""}`}>
+            <div className={styles.inputWrapper}>
+                <input type={type} name={name} placeholder="no-label" {...field} {...rest} />
+                <label>{label}</label>
+            </div>
+            {
+                fieldState.invalid ? <div className={styles.error}>{fieldState?.error?.message}</div> : null
+            }
+        </div>
+    )
+}
+
+Input.propTypes = {
+    label: PropTypes.string,
+    value: PropTypes.any,
+    onChange: PropTypes.func,
+    errorMessage: PropTypes.string,
+    name: PropTypes.string,
+    type: PropTypes.oneOf(["text", "password"])
+}
+
+export { Input };
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,12 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import useDocumentTitle from "@hooks/useDocumentTitle";
+import { Input } from "@components/Input/Input";
+import Button from "@components/Button";
+import styles from "./Login.module.scss";
+import { useForm } from "react-hook-form";
+
+const FORM_DATA = {
+    email: "",
+    password: ""
+}
 
 export function Login() {
+    const { formState: { errors }, control, handleSubmit } = useForm({
+        defaultValues: {
+            ...FORM_DATA
+        },
+        mode: "onChange"
+    });
     useDocumentTitle("User Login | Sabka Bazaar");
 
+    const onSubmit = (data) => {
+        console.log(data);
+    };
+
     return (
-        <>
-            Login
-        </>
+        <div className={styles.loginWrapper}>
+            <div className={styles.loginDescription}>
+                <h2 className={styles.heading}>Login</h2>
+                <p className={styles.description}>Get Access to your Orders, Wishlist and Recommendations</p>
+            </div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <Input
+                    control={control}
+                    label="Email"
+                    name="email"
+                    type="text"
+                    rules={{ required: "Email Address is required" }}
+                    aria-invalid={errors.email ? "true" : "false"}
+                />
+                <Input
+                    control={control}
+                    label="Password"
+                    name="password"
+                    type="text"
+                    rules={{ required: "Password is required" }}
+                    aria-invalid={errors.email ? "true" : "false"}
+                />
+                <div className={styles.buttonWrapper}>
+                    <Button>Login</Button>
+                </div>
+            </form>
+        </div>
     )
 }
