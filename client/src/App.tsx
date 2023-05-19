46673a39aad59f999ca89fdf1461fab8ea319242
Prakash Kumar <prakumar162@Prakashs-MacBook-Pro.local>
2023-05-19 11:02:58 +0530

    feat: initial commit + client setup

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import Logo from "../../static/images/logo.png";
+
+import "./App.scss";
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={Logo} className="logo" alt="Vite logo" />
+      </div>
+      <h1>Sabka Bazaar</h1>
+    </>
+  );
+}
+
+export default App;
