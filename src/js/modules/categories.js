d979a337827ae639d879d1057d77dd52dbec0f0e
Navdeep Singh <navdeep.er@gmail.com>
2023-05-29 12:33:38 +0530

    Resetting develop branch

--- src/js/modules/categories.js
+++
@@ -1,5 +0,0 @@
-export async function getCategories() {
-  let response = await fetch("http://localhost:5000/categories");
-  let results = await response.json();
-  return results.filter((e) => e.enabled).sort((a, b) => a.order - b.order);
-}
